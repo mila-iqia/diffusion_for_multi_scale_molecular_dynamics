c38e3c9de9e6451518579cccd6994849d1fc20e3
Simon Blackburn <simon.blackburn@mila.quebec>
2024-02-20 14:58:31 -0500

    myst-parser version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         'gitpython==3.1.27',
         'jupyter==1.0.0',
         'jinja2==3.1.2',
-        'myst-parser==0.18.0',
+        'myst-parser==2.0.0',
         'orion>=0.2.4.post1',
         'pyyaml==6.0',
         'pytest==7.1.2',
