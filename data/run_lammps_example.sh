9550c930d8c89ea1d88a83c1498cc0fa6fa13e8b
Simon Blackburn <simon.blackburn@mila.quebec>
2024-02-26 08:28:11 -0500

    cleaning files from error in rebase

--- data/run_lammps_example.sh
+++ data/run_lammps_example.sh
@@ -8,6 +8,5 @@ lmp < lammps_input_example.lammps -v STEP 10 -v T $TEMPERATURE -v S $BOX_SIZE
 
 # extract the thermodynamic outputs in a yaml file
 egrep  '^(keywords:|data:$|---$|\.\.\.$|  - \[)' log.lammps > log.yaml
-=======
+
 lmp < lammps_input_example.lammps -v STEP 10 -v T $TEMPERATURE -v S $BOX_SIZE
->>>>>>> 5927511b163780d0be25d5db4a0eb8868b12f8b2
