05d14834799e5ec52b027907a2fc72a0d04334c8
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-03-01 20:24:58 -0500

    Plotting evidence of convergence.

--- crystal_diffusion/analysis/target_score_analysis.py
+++ crystal_diffusion/analysis/target_score_analysis.py
@@ -1,3 +1,8 @@
+"""Target score analysis.
+
+This script computes and plots the target score for various values of sigma, showing
+that the 'smart' implementation converges quickly and is equal to the expected brute force value.
+"""
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
@@ -10,39 +15,61 @@ plt.style.use(PLOT_STYLE_PATH)
 
 if __name__ == '__main__':
 
-    kmax = 2
     list_u = np.linspace(0, 1, 101)[:-1]
-    sigma_factor = 0.1
-    sigma = sigma_factor * SIGMA_THRESHOLD
-
-    list_scores_brute = np.array([get_expected_sigma_normalized_score_brute_force(u, sigma) for u in list_u])
-
     relative_positions = torch.from_numpy(list_u)
-    sigmas = torch.ones_like(relative_positions) * sigma
-    list_scores = get_sigma_normalized_score(relative_positions, sigmas, kmax=kmax).numpy()
-    error = list_scores - list_scores_brute
 
-    fig = plt.figure(figsize=PLEASANT_FIG_SIZE)
-    ax1 = fig.add_subplot(211)
-    ax2 = fig.add_subplot(212)
+    # A first figure to compare the "smart" and the "brute force" results
+    fig1 = plt.figure(figsize=PLEASANT_FIG_SIZE)
+    fig1.suptitle("Smart vs. brute force scores")
 
-    ax1.set_title(f'$\\sigma$ = {sigma_factor} $\\sigma_{{th}}$')
-    ax1.plot(list_u, list_scores_brute, 'b--', lw=4, label='brute force')
-    ax1.plot(list_u, list_scores, 'g-', lw=2, label='smart')
+    kmax = 4
+    ax1 = fig1.add_subplot(121)
+    ax2 = fig1.add_subplot(122)
 
-    ax2.plot(list_u, error, 'r-')
+    for sigma_factor, color in zip([0.1, 1., 2.], ['r', 'g', 'b']):
+        sigma = sigma_factor * SIGMA_THRESHOLD
+
+        sigmas = torch.ones_like(relative_positions) * sigma
+        list_scores_brute = np.array([get_expected_sigma_normalized_score_brute_force(u, sigma) for u in list_u])
+        list_scores = get_sigma_normalized_score(relative_positions, sigmas, kmax=kmax).numpy()
+        error = list_scores - list_scores_brute
+
+        ax1.plot(list_u, list_scores_brute, '--', c=color, lw=4, label='brute force')
+        ax1.plot(list_u, list_scores, '-', c=color, lw=2, label='smart')
+
+        ax2.plot(list_u, error, '-', c=color, label=f'$\\sigma$ = {sigma_factor} $\\sigma_{{th}}$')
 
     ax1.set_ylabel('$\\sigma^2\\times S$')
-
     ax2.set_xlabel('u')
     ax2.set_ylabel('Error')
-
-    ax1.legend(loc=0)
 
     for ax in [ax1, ax2]:
         ax.set_xlabel('u')
         ax.set_xlim([0, 1])
+        ax.legend(loc=0)
 
-    fig.tight_layout()
+    fig1.tight_layout()
+
+    # A second figure to show convergence with kmax
+    fig2 = plt.figure(figsize=PLEASANT_FIG_SIZE)
+    fig2.suptitle("Convergence with kmax")
+
+    ax3 = fig2.add_subplot(111)
+
+    sigmas = torch.linspace(0.01, 2., 20) * SIGMA_THRESHOLD
+
+    u = 0.6
+    relative_positions = torch.ones_like(sigmas) * u
+
+    ms = 8
+    for kmax, color in zip([1, 2, 3, 4], ['r', 'g', 'b', 'k']):
+        list_scores = get_sigma_normalized_score(relative_positions, sigmas, kmax=kmax).numpy()
+        ax3.plot(sigmas, list_scores, 'o-', ms=ms, c=color, lw=2, alpha=0.25, label=f'kmax = {kmax}')
+        ms = 0.75 * ms
+
+    ax3.set_xlabel('$\\sigma$')
+    ax3.set_ylabel(f'$\\sigma^2 \\times S(u={u})$')
+    ax3.set_xlim([0., 1.1 * sigma.max()])
+    ax3.legend(loc=0)
 
     plt.show()
