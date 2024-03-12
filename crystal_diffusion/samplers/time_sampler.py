a1a376bc50751432233041f709c2fd80bd85ea58
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-03-12 15:35:38 -0400

    Class to sample time steps.

---
+++ crystal_diffusion/samplers/time_sampler.py
@@ -0,0 +1,83 @@
+from dataclasses import dataclass
+from typing import Tuple
+
+import torch
+
+
+@dataclass
+class TimeParameters:
+    """Time sampling parameters."""
+    total_time_steps: int
+    random_seed: int = 1234
+
+
+class TimeSampler:
+    """Time Sampler.
+
+    This class will produce time step samples as needed.
+    The times will be normalized to be between 0 and 1.
+    """
+    def __init__(self, time_parameters: TimeParameters):
+        """Init method.
+
+        Args:
+            time_parameters: parameters needed to instantiate the time sampler.
+        """
+        self.time_parameters = time_parameters
+        self._time_step_array = torch.linspace(0, 1, time_parameters.total_time_steps)
+
+        self._maximum_index = time_parameters.total_time_steps - 1
+        self._minimum_index = 1  # we don't want to sample "0".
+        self._rng = torch.manual_seed(time_parameters.random_seed)
+
+    def get_random_time_step_indices(self, shape: Tuple[int]) -> torch.Tensor:
+        """Random time step indices.
+
+        Generate random indices that correspond to valid time steps.
+        This sampling avoids index "0", which corresponds to time "0".
+
+        Args:
+            shape: shape of the random index array.
+
+        Returns:
+            time_step_indices: random time step indices in a tensor of shape "shape".
+        """
+        random_indices = torch.randint(self._minimum_index,
+                                       self._maximum_index + 1,  # +1 because the maximum value is not sampled
+                                       size=shape,
+                                       generator=self._rng)
+        return random_indices
+
+    def get_time_steps(self, indices: torch.Tensor) -> torch.Tensor:
+        """Get time steps.
+
+        Extract the time steps from the internal data structure, given desired indices.
+
+        Args:
+            indices: the time step indices. Must be between 0 and "total_number_of_time_steps" - 1.
+
+        Returns:
+            time_steps: random time step indices in a tensor of shape "shape".
+        """
+        assert torch.all(indices >= 0), "indices must be non-negative"
+        assert torch.all(indices <= self._maximum_index), \
+            f"indices must be smaller than or equal to {self._maximum_index}"
+        return self._time_step_array.take(indices)
+
+    def get_forward_iterator(self):
+        """Get forward iterator.
+
+        Returns:
+            forward_iterator: an iterator over (index, time_step) that iterates over increasing values.
+        """
+        return iter(enumerate(self._time_step_array))
+
+    def get_backward_iterator(self):
+        """Get backward iterator.
+
+        Returns:
+            backward_iterator: an iterator over (index, time_step) that iterates over decreasing values.
+        """
+        maximum_index = len(self._time_step_array) - 1
+        indices = range(maximum_index, -1, -1)
+        return zip(indices, self._time_step_array.flip(dims=(0,)))
---
+++ tests/samplers/test_time_sampler.py
@@ -0,0 +1,66 @@
+import pytest
+import torch
+
+from crystal_diffusion.samplers.time_sampler import TimeParameters, TimeSampler
+
+
+@pytest.mark.parametrize('total_time_steps', [3, 10, 17])
+class TestTimeSampler:
+    @pytest.fixture()
+    def time_parameters(self, total_time_steps):
+        time_parameters = TimeParameters(total_time_steps=total_time_steps, random_seed=0)
+        return time_parameters
+
+    @pytest.fixture()
+    def time_sampler(self, time_parameters):
+        return TimeSampler(time_parameters)
+
+    @pytest.mark.parametrize('shape', [(100,), (3, 8), (1, 2, 3)])
+    def test_random_time_step_indices(self, time_sampler, shape, total_time_steps):
+        computed_time_step_indices = time_sampler.get_random_time_step_indices(shape)
+        assert computed_time_step_indices.shape == shape
+        assert torch.all(computed_time_step_indices > 0)
+        assert torch.all(computed_time_step_indices <= total_time_steps)
+
+    @pytest.mark.parametrize('shape', [(100,), (3, 8), (1, 2, 3)])
+    def test_random_time_steps(self, time_sampler, shape, total_time_steps):
+        indices = time_sampler.get_random_time_step_indices(shape)
+        computed_time_steps = time_sampler.get_time_steps(indices)
+        assert computed_time_steps.shape == shape
+
+        assert torch.all(computed_time_steps > 0.0)
+        assert torch.all(computed_time_steps <= 1.0)
+
+        # The random values should come from an array of the form [dt, 2 dt, 3 dt,.... N dt]
+        delta_time = 1. / (total_time_steps - 1.)
+        number_of_time_segments = torch.round(computed_time_steps / delta_time)
+        expected_time_steps = number_of_time_segments * delta_time
+
+        assert torch.all(torch.isclose(expected_time_steps, computed_time_steps))
+
+    def test_forward_iterator(self, time_sampler, total_time_steps):
+
+        expected_index = 0
+        expected_time = torch.tensor(0.0).to(torch.double)
+
+        dt = torch.tensor(1. / (total_time_steps - 1.)).to(torch.double)
+
+        for index, time in time_sampler.get_forward_iterator():
+            assert index == expected_index
+            expected_index += 1
+
+            assert torch.isclose(time, expected_time.float())
+            expected_time += dt
+
+    def test_backward_iterator(self, time_sampler, total_time_steps):
+
+        expected_index = total_time_steps - 1
+        expected_time = torch.tensor(1.0).to(torch.double)
+        dt = torch.tensor(1. / (total_time_steps - 1.)).to(torch.double)
+
+        for index, time in time_sampler.get_backward_iterator():
+            assert index == expected_index
+            expected_index -= 1
+
+            assert torch.isclose(time, expected_time.float())
+            expected_time -= dt
