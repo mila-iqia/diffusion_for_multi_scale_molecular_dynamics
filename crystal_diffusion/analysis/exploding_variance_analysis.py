2d4d975a903ba18f95846f0347ed6fd3ea63a321
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-03-13 05:43:49 -0400

    Plot the target noise, too.

--- crystal_diffusion/analysis/exploding_variance_analysis.py
+++ crystal_diffusion/analysis/exploding_variance_analysis.py
@@ -11,6 +11,8 @@ from crystal_diffusion.analysis import PLEASANT_FIG_SIZE, PLOT_STYLE_PATH
 from crystal_diffusion.samplers.time_sampler import TimeParameters, TimeSampler
 from crystal_diffusion.samplers.variance_sampler import (
     ExplodingVarianceSampler, VarianceParameters)
+from crystal_diffusion.score.wrapped_gaussian_score import \
+    get_sigma_normalized_score
 
 plt.style.use(PLOT_STYLE_PATH)
 
@@ -32,9 +34,9 @@ if __name__ == '__main__':
     fig1 = plt.figure(figsize=PLEASANT_FIG_SIZE)
     fig1.suptitle("Noise Schedule")
 
-    kmax = 4
-    ax1 = fig1.add_subplot(121)
-    ax2 = fig1.add_subplot(122)
+    ax1 = fig1.add_subplot(221)
+    ax2 = fig1.add_subplot(223)
+    ax3 = fig1.add_subplot(122)
 
     ax1.plot(times, sigmas, '-', c='k', lw=2)
     ax2.plot(times[1:], gs, '-', c='k', lw=2)
@@ -48,6 +50,26 @@ if __name__ == '__main__':
 
     ax1.set_title("$\\sigma$ schedule")
     ax2.set_title("g schedule")
+
+    relative_positions = torch.linspace(0, 1, 101)[:-1]
+
+    kmax = 4
+    indices = torch.tensor([1, 250, 750, 999])
+    times = time_sampler.get_time_steps(indices)
+    sigmas = torch.sqrt(variance_sampler.get_variances(indices))
+    gs_squared = variance_sampler.get_g_squared_factors(indices)
+
+    for t, sigma in zip(times, sigmas):
+        target_scores = get_sigma_normalized_score(relative_positions,
+                                                   torch.ones_like(relative_positions) * sigma,
+                                                   kmax=kmax)
+        ax3.plot(relative_positions, sigma * target_scores, label=f"t = {t:3.2f}")
+
+    ax3.set_title("Target Noise")
+    ax3.set_xlabel("relative position, u")
+    ax3.set_ylabel("$\\sigma(t) \\times S(u, t)$")
+    ax3.legend(loc=0)
+    ax3.set_xlim([-0.01, 1.01])
 
     fig1.tight_layout()
 
