64470d3607275b7a5e6c94cd014c3f760395498d
Simon Blackburn <simon.blackburn@mila.quebec>
2024-03-15 14:45:23 -0400

    isort

--- tests/analysis/test_ovito_visualisation.py
+++ tests/analysis/test_ovito_visualisation.py
@@ -5,7 +5,8 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from crystal_diffusion.analysis.ovito_visualisation import get_lattice_from_lammps, mtp_predictions_to_ovito
+from crystal_diffusion.analysis.ovito_visualisation import (
+    get_lattice_from_lammps, mtp_predictions_to_ovito)
 
 
 @pytest.fixture
