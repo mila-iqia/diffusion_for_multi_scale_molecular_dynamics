61ef471a5e0ef26731510d2a6e094bb0c4143fc1
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-03-16 17:24:23 -0400

    Fix plotting script.

--- crystal_diffusion/analysis/exploding_variance_analysis.py
+++ crystal_diffusion/analysis/exploding_variance_analysis.py
@@ -8,9 +8,8 @@ import torch
 
 from crystal_diffusion import ANALYSIS_RESULTS_DIR
 from crystal_diffusion.analysis import PLEASANT_FIG_SIZE, PLOT_STYLE_PATH
-from crystal_diffusion.samplers.time_sampler import TimeParameters, TimeSampler
 from crystal_diffusion.samplers.variance_sampler import (
-    ExplodingVarianceSampler, VarianceParameters)
+    ExplodingVarianceSampler, NoiseParameters)
 from crystal_diffusion.score.wrapped_gaussian_score import \
     get_sigma_normalized_score
 
@@ -18,19 +17,11 @@ plt.style.use(PLOT_STYLE_PATH)
 
 if __name__ == '__main__':
 
-    variance_parameters = VarianceParameters()
-    time_parameters = TimeParameters(total_time_steps=1000)
+    noise_parameters = NoiseParameters(total_time_steps=1000)
+    variance_sampler = ExplodingVarianceSampler(noise_parameters=noise_parameters)
 
-    time_sampler = TimeSampler(time_parameters=time_parameters)
-    variance_sampler = ExplodingVarianceSampler(variance_parameters=variance_parameters,
-                                                time_sampler=time_sampler)
+    noise = variance_sampler.get_all_noise()
 
-    indices = torch.arange(time_parameters.total_time_steps)
-    times = time_sampler.get_time_steps(indices)
-    sigmas = torch.sqrt(variance_sampler.get_variances(indices))
-    gs = torch.sqrt(variance_sampler.get_g_squared_factors(indices[1:]))
-
-    # A first figure to compare the "smart" and the "brute force" results
     fig1 = plt.figure(figsize=PLEASANT_FIG_SIZE)
     fig1.suptitle("Noise Schedule")
 
@@ -38,8 +29,8 @@ if __name__ == '__main__':
     ax2 = fig1.add_subplot(223)
     ax3 = fig1.add_subplot(122)
 
-    ax1.plot(times, sigmas, '-', c='k', lw=2)
-    ax2.plot(times[1:], gs, '-', c='k', lw=2)
+    ax1.plot(noise.time, noise.sigma, '-', c='k', lw=2)
+    ax2.plot(noise.time[1:], noise.g[1:], '-', c='k', lw=2)
 
     ax1.set_ylabel('$\\sigma(t)$')
     ax2.set_ylabel('$g(t)$')
@@ -55,9 +46,10 @@ if __name__ == '__main__':
 
     kmax = 4
     indices = torch.tensor([1, 250, 750, 999])
-    times = time_sampler.get_time_steps(indices)
-    sigmas = torch.sqrt(variance_sampler.get_variances(indices))
-    gs_squared = variance_sampler.get_g_squared_factors(indices)
+
+    times = noise.time.take(indices)
+    sigmas = noise.sigma.take(indices)
+    gs_squared = noise.g_squared.take(indices)
 
     for t, sigma in zip(times, sigmas):
         target_scores = get_sigma_normalized_score(relative_positions,
@@ -65,7 +57,7 @@ if __name__ == '__main__':
                                                    kmax=kmax)
         ax3.plot(relative_positions, sigma * target_scores, label=f"t = {t:3.2f}")
 
-    ax3.set_title("Target Noise")
+    ax3.set_title("Target Normalized Score")
     ax3.set_xlabel("relative position, u")
     ax3.set_ylabel("$\\sigma(t) \\times S(u, t)$")
     ax3.legend(loc=0)
