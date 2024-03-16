ee766b91b7257bd31e14d02115dfa5273f4ae08f
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-03-16 17:00:41 -0400

    A cleaner way to draw noise samples.

--- tests/samplers/test_variance_sampler.py
+++ tests/samplers/test_variance_sampler.py
@@ -1,89 +1,101 @@
 import pytest
 import torch
 
-from crystal_diffusion.samplers.time_sampler import TimeParameters, TimeSampler
 from crystal_diffusion.samplers.variance_sampler import (
-    ExplodingVarianceSampler, VarianceParameters)
+    ExplodingVarianceSampler, NoiseParameters)
 
 
-@pytest.mark.parametrize('total_time_steps', [3, 10, 17])
+@pytest.mark.parametrize("total_time_steps", [3, 10, 17])
 class TestExplodingVarianceSampler:
     @pytest.fixture()
-    def time_parameters(self, total_time_steps):
-        time_parameters = TimeParameters(total_time_steps=total_time_steps, random_seed=0)
-        return time_parameters
+    def noise_parameters(self, total_time_steps):
+        return NoiseParameters(total_time_steps=total_time_steps)
 
     @pytest.fixture()
-    def time_sampler(self, time_parameters):
-        return TimeSampler(time_parameters)
+    def variance_sampler(self, noise_parameters):
+        return ExplodingVarianceSampler(noise_parameters=noise_parameters)
 
     @pytest.fixture()
-    def variance_parameters(self, time_sampler):
-        return VarianceParameters()
+    def expected_times(self, total_time_steps):
+        times = torch.linspace(0.0, 1.0, total_time_steps)
+        return times
 
     @pytest.fixture()
-    def variance_sampler(self, variance_parameters, time_sampler):
-        return ExplodingVarianceSampler(variance_parameters=variance_parameters,
-                                        time_sampler=time_sampler)
+    def expected_sigmas(self, expected_times, noise_parameters):
+        smin = noise_parameters.sigma_min
+        smax = noise_parameters.sigma_max
 
-    @pytest.fixture()
-    def expected_variances(self, variance_parameters, time_sampler, time_parameters):
-
-        times = torch.linspace(0., 1., time_parameters.total_time_steps)
-
-        smin = variance_parameters.sigma_min
-        smax = variance_parameters.sigma_max
-
-        sigmas = smin**(1. - times) * smax ** times
-        variances = sigmas**2
-        return variances
+        sigmas = smin ** (1.0 - expected_times) * smax**expected_times
+        return sigmas
 
     @pytest.fixture()
     def indices(self, time_sampler, shape):
         return time_sampler.get_random_time_step_indices(shape)
 
-    @pytest.fixture()
-    def expected_variances_by_index(self, expected_variances, indices):
-        result = []
-        for i in indices.flatten():
-            result.append(expected_variances[i])
-        return torch.tensor(result).reshape(indices.shape)
+    def test_time_array(self, variance_sampler, expected_times):
+        assert torch.all(torch.isclose(variance_sampler._time_array, expected_times))
 
-    def test_sigma_square_array(self, variance_sampler, expected_variances):
-        assert torch.all(torch.isclose(variance_sampler._sigma_square_array, expected_variances))
+    def test_sigma_and_sigma_squared_arrays(self, variance_sampler, expected_sigmas):
+        assert torch.all(torch.isclose(variance_sampler._sigma_array, expected_sigmas))
+        assert torch.all(
+            torch.isclose(variance_sampler._sigma_squared_array, expected_sigmas**2)
+        )
 
-    def test_g_square_array(self, variance_sampler, expected_variances):
+    def test_g_and_g_square_array(self, variance_sampler, expected_sigmas):
+        expected_sigmas_square = expected_sigmas**2
 
-        expected_g_squared_array = [float('nan')]
-        for sigma2_t, sigma2_tm1 in zip(expected_variances[1:], expected_variances[:-1]):
+        expected_g_squared_array = [float("nan")]
+        for sigma2_t, sigma2_tm1 in zip(
+            expected_sigmas_square[1:], expected_sigmas_square[:-1]
+        ):
             g2 = sigma2_t - sigma2_tm1
             expected_g_squared_array.append(g2)
 
         expected_g_squared_array = torch.tensor(expected_g_squared_array)
+        expected_g_array = torch.sqrt(expected_g_squared_array)
 
-        assert torch.isnan(variance_sampler._g_square_array[0])
-        assert torch.all(torch.isclose(variance_sampler._g_square_array[1:], expected_g_squared_array[1:]))
+        assert torch.isnan(variance_sampler._g_array[0])
+        assert torch.isnan(variance_sampler._g_squared_array[0])
+        assert torch.all(
+            torch.isclose(variance_sampler._g_array[1:], expected_g_array[1:])
+        )
+        assert torch.all(
+            torch.isclose(
+                variance_sampler._g_squared_array[1:], expected_g_squared_array[1:]
+            )
+        )
 
-    @pytest.mark.parametrize('shape', [(100,), (3, 8), (1, 2, 3)])
-    def test_get_variances(self, variance_sampler, indices, expected_variances_by_index):
-        computed_variances = variance_sampler.get_variances(indices)
-        assert torch.all(torch.isclose(computed_variances, expected_variances_by_index))
+    def test_get_random_time_step_indices(self, variance_sampler, total_time_steps):
+        # Check that we never sample zero.
+        random_indices = variance_sampler._get_random_time_step_indices(shape=(1000,))
+        assert torch.all(random_indices > 0)
+        assert torch.all(random_indices < total_time_steps)
 
-    def test_get_g_squared_factors(self, variance_sampler, expected_variances):
+    @pytest.mark.parametrize("batch_size", [1, 10, 100])
+    def test_get_random_noise_parameter_sample(
+        self, mocker, variance_sampler, batch_size
+    ):
+        random_indices = variance_sampler._get_random_time_step_indices(shape=(1000,))
+        mocker.patch.object(
+            variance_sampler,
+            "_get_random_time_step_indices",
+            return_value=random_indices,
+        )
 
-        indices = []
-        expected_g_squared_factors = []
-        previous_variance = expected_variances[0]
+        noise_sample = variance_sampler.get_random_noise_sample(batch_size)
 
-        for index, current_variance in enumerate(expected_variances[1:], 1):
-            indices.append(index)
-            current_variance = expected_variances[index]
-            g2 = current_variance - previous_variance
-            expected_g_squared_factors.append(g2)
-            previous_variance = current_variance
-        expected_g_squared_factors = torch.tensor(expected_g_squared_factors)
+        expected_times = variance_sampler._time_array.take(random_indices)
+        expected_sigmas = variance_sampler._sigma_array.take(random_indices)
+        expected_sigmas_squared = variance_sampler._sigma_squared_array.take(
+            random_indices
+        )
+        expected_gs = variance_sampler._g_array.take(random_indices)
+        expected_gs_squared = variance_sampler._g_squared_array.take(random_indices)
 
-        indices = torch.tensor(indices)
-        computed_g_squared_factors = variance_sampler.get_g_squared_factors(indices)
-
-        assert torch.all(torch.isclose(computed_g_squared_factors, expected_g_squared_factors))
+        assert torch.all(torch.isclose(noise_sample.time, expected_times))
+        assert torch.all(torch.isclose(noise_sample.sigma, expected_sigmas))
+        assert torch.all(
+            torch.isclose(noise_sample.sigma_squared, expected_sigmas_squared)
+        )
+        assert torch.all(torch.isclose(noise_sample.g, expected_gs))
+        assert torch.all(torch.isclose(noise_sample.g_squared, expected_gs_squared))
