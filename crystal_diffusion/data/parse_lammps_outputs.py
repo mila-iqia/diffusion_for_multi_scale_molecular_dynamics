218170b4a5bd991b9fe279be54833d34c59735a6
Simon Blackburn <simon.blackburn@mila.quebec>
2024-03-18 11:07:13 -0400

    adding box info to lammps output parser

--- crystal_diffusion/data/parse_lammps_outputs.py
+++ crystal_diffusion/data/parse_lammps_outputs.py
@@ -35,6 +35,9 @@ def parse_lammps_output(lammps_dump: str, lammps_thermo_log: str, output_name: O
             if 'id' not in doc['keywords']:  # sanity check
                 raise ValueError('id should be in LAMMPS dump file')
             atoms_info = defaultdict(list)  # store information on atoms positions and forces here
+            # get periodic box information
+            box_lim = [l_vec[1] for l_vec in doc['box']]
+            pd_data['box'].append(box_lim)  # add to dataframe
             for data in doc['data']:  # loop over the atoms to get their positions and forces
                 for key, v in zip(doc['keywords'], data):
                     if key not in ['id', 'type', 'x', 'y', 'z', 'fx', 'fy', 'fz']:
