50d9d1cd69ec416411fbb5b43beabd7112826d72
Simon Blackburn <simon.blackburn@mila.quebec>
2024-03-22 13:40:41 -0400

    update to bash script

--- data/si_diffusion_v1/create_data.sh
+++ data/si_diffusion_v1/create_data.sh
@@ -9,27 +9,28 @@ NVALID_RUN=5
 
 NRUN=$(($NTRAIN_RUN + $NVALID_RUN))
 
-for SEED in {1..$NRUN}
+for SEED in $(seq 1 $NRUN);
 do
   if [ "$SEED" -le $NTRAIN_RUN ]; then
     MODE="train"
   else
     MODE="valid"
   fi
+  echo $MODE $SEED
   mkdir -p "${MODE}_run_${SEED}"
   cd "${MODE}_run_${SEED}"
-  lmp < ../in.si.lammps -v STEP $(($STEP + $CROP)) -v T $TEMPERATURE -v S $BOX_SIZE
+  lmp < ../in.si.lammps -v STEP $(($STEP + $CROP)) -v T $TEMPERATURE -v S $BOX_SIZE -v SEED $SEED
 
   # extract the thermodynamic outputs in a yaml file
   egrep  '^(keywords:|data:$|---$|\.\.\.$|  - \[)' log.lammps > thermo_log.yaml
 
   mkdir -p "uncropped_outputs"
-  mv dump.si-${T}-${S}.yaml uncropped_outputs/
+  mv "dump.si-${TEMPERATURE}-${BOX_SIZE}.yaml" uncropped_outputs/
   mv thermo_log.yaml uncropped_outputs/
 
-  python ../../data/crop_lammps_outputs.py \
-      --lammps_yaml uncropped_outputs/dump.si-${T}-${S}.yaml \
-      --lammps_thermo uncropped_outputs/thermo_log.yaml \
+  python ../../crop_lammps_outputs.py \
+      --lammps_yaml "uncropped_outputs/dump.si-${TEMPERATURE}-${BOX_SIZE}.yaml" \
+      --lammps_thermo "uncropped_outputs/thermo_log.yaml" \
       --crop $CROP \
       --output_dir ./
 
