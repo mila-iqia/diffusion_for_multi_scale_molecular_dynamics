5cfcc9ec86c232f2f28dcd02e7c3112284915c03
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-03-25 16:07:53 -0400

    Interface for PC sampler.

---
+++ tests/samplers/test_predictor_corrector_position_sampler.py
@@ -0,0 +1,78 @@
+import pytest
+import torch
+
+from crystal_diffusion.samplers.noisy_position_sampler import \
+    map_positions_to_unit_cell
+from crystal_diffusion.samplers.predictor_corrector_position_sampler import \
+    PredictorCorrectorPositionSampler
+
+
+class FakePCSampler(PredictorCorrectorPositionSampler):
+    """A dummy PC sampler for the purpose of testing."""
+
+    def __init__(
+        self,
+        number_of_discretization_steps: int,
+        number_of_corrector_steps: int,
+        initial_sample: torch.Tensor,
+    ):
+        super().__init__(number_of_discretization_steps, number_of_corrector_steps)
+        self.initial_sample = initial_sample
+
+    def initialize(self, number_of_samples: int):
+        return self.initial_sample
+
+    def predictor_step(self, x_ip1: torch.Tensor) -> torch.Tensor:
+        return 1.2 * x_ip1 + 3.4
+
+    def corrector_step(self, x_i: torch.Tensor) -> torch.Tensor:
+        return 0.56 * x_i + 7.89
+
+
+@pytest.mark.parametrize("number_of_samples", [4])
+@pytest.mark.parametrize("number_of_atoms", [8])
+@pytest.mark.parametrize("spatial_dimension", [2, 3])
+@pytest.mark.parametrize("number_of_discretization_steps", [1, 5, 10])
+@pytest.mark.parametrize("number_of_corrector_steps", [0, 1, 2])
+class TestPredictorCorrectorPositionSampler:
+    @pytest.fixture(scope="class", autouse=True)
+    def set_random_seed(self):
+        torch.manual_seed(1234567)
+
+    @pytest.fixture
+    def initial_sample(self, number_of_samples, number_of_atoms, spatial_dimension):
+        return torch.rand(number_of_samples, number_of_atoms, spatial_dimension)
+
+    @pytest.fixture
+    def sampler(
+        self, number_of_discretization_steps, number_of_corrector_steps, initial_sample
+    ):
+        sampler = FakePCSampler(
+            number_of_discretization_steps, number_of_corrector_steps, initial_sample
+        )
+        return sampler
+
+    @pytest.fixture
+    def expected_samples(
+        self,
+        sampler,
+        initial_sample,
+        number_of_discretization_steps,
+        number_of_corrector_steps,
+    ):
+        # the value of i doesn't matter; we can traverse it in whatever order.
+        list_i = list(range(number_of_discretization_steps))
+        list_j = list(range(number_of_corrector_steps))
+
+        noisy_sample = map_positions_to_unit_cell(initial_sample)
+        x_ip1 = noisy_sample
+        for _ in list_i:
+            xi = map_positions_to_unit_cell(sampler.predictor_step(x_ip1))
+            for _ in list_j:
+                xi = map_positions_to_unit_cell(sampler.corrector_step(xi))
+            x_ip1 = xi
+        return xi
+
+    def test_sample(self, sampler, number_of_samples, expected_samples):
+        computed_samples = sampler.sample(number_of_samples)
+        torch.testing.assert_allclose(expected_samples, computed_samples)
