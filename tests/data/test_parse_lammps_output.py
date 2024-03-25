0296798e4e70d322b37ab15e9c6376ad4d0ed208
Simon Blackburn <simon.blackburn@mila.quebec>
2024-03-25 11:47:43 -0400

    update to test as well

--- tests/data/test_parse_lammps_output.py
+++ tests/data/test_parse_lammps_output.py
@@ -22,13 +22,17 @@ def fake_lammps_yaml(tmpdir):
     # fake LAMMPS output file with 4 MD steps in 1D for 3 atoms
     yaml_content = [
         {'keywords': ['id', 'type', 'x', 'fx'],
-         'data': [[0, 1, 0.1, 0.01], [1, 2, 0.2, 0.02], [2, 1, 0.3, 0.03]]},
+         'data': [[0, 1, 0.1, 0.01], [1, 2, 0.2, 0.02], [2, 1, 0.3, 0.03]],
+         'box': [[0, 0.6], [0, 1.6], [0, 2.6]]},
         {'keywords': ['id', 'type', 'x', 'fx'],
-         'data': [[0, 1, 1.1, 1.01], [1, 2, 1.2, 1.02], [2, 1, 1.3, 1.03]]},
+         'data': [[0, 1, 1.1, 1.01], [1, 2, 1.2, 1.02], [2, 1, 1.3, 1.03]],
+         'box': [[0, 0.6], [0, 1.6], [0, 2.6]]},
         {'keywords': ['id', 'type', 'x', 'fx'],
-         'data': [[0, 1, 2.1, 2.01], [1, 2, 2.2, 2.02], [2, 1, 2.3, 2.03]]},
+         'data': [[0, 1, 2.1, 2.01], [1, 2, 2.2, 2.02], [2, 1, 2.3, 2.03]],
+         'box': [[0, 0.6], [0, 1.6], [0, 2.6]]},
         {'keywords': ['id', 'type', 'x', 'fx'],
-         'data': [[0, 1, 3.1, 3.01], [1, 2, 3.2, 3.02], [2, 1, 3.3, 3.03]]},
+         'data': [[0, 1, 3.1, 3.01], [1, 2, 3.2, 3.02], [2, 1, 3.3, 3.03]],
+         'box': [[0, 0.6], [0, 1.6], [0, 2.6]]},
     ]
     file = os.path.join(tmpdir, 'fake_lammps_dump.yaml')
     generate_fake_yaml(file, yaml_content)
@@ -40,7 +44,7 @@ def fake_thermo_yaml(tmpdir):
     # fake LAMMPS thermo file with 4 MD steps
     yaml_content = {
         'keywords': ['KinEng', 'PotEng'],
-        'data': [[0.4, 0.5], [1.4, 1.5], [2.4, 2.5], [3.4, 3.5]]
+        'data': [[0.4, 0.5], [1.4, 1.5], [2.4, 2.5], [3.4, 3.5]],
     }
     file = os.path.join(tmpdir, 'fake_lammps_thermo.yaml')
     generate_fake_yaml(file, yaml_content, multiple_docs=False)
@@ -58,7 +62,7 @@ def test_parse_lammps_outputs(fake_lammps_yaml, fake_thermo_yaml, tmpdir):
 
     assert len(df) == 4
 
-    for i, v in enumerate(['id', 'type', 'x', 'fx', 'energy']):
+    for i, v in enumerate(['id', 'type', 'x', 'fx', 'energy', 'box']):
         assert v in df.keys()
         for x in range(4):
             if v == 'id':
@@ -69,5 +73,7 @@ def test_parse_lammps_outputs(fake_lammps_yaml, fake_thermo_yaml, tmpdir):
                 assert np.allclose(df[v][x], [x + 0.1 * y for y in range(1, 4)])
             elif v == 'fx':
                 assert np.allclose(df[v][x], [x + 0.01 * y for y in range(1, 4)])
-            else:  # v == 'energy'
+            elif v == 'energy':
                 assert np.allclose(df[v][x], [2 * x + 0.9])
+            else:  # v == 'box'
+                assert np.allclose(df[v][x], [x + 0.6 for x in range(3)])
