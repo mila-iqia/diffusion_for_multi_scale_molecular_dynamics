04c17927be926c55ef3dcd1b234c8cf52a066bc7
Simon Blackburn <simon.blackburn@mila.quebec>
2024-03-28 14:19:06 -0400

    end2end diffusion model training

--- crystal_diffusion/models/model_loader.py
+++ crystal_diffusion/models/model_loader.py
@@ -1,28 +1,47 @@
+"""Functions to instantiate a model based on the provided hyperparameters."""
 import logging
+from typing import Any, AnyStr, Dict
 
-from crystal_diffusion.models.score_network import MLPScoreNetwork
+from crystal_diffusion.models.optimizer import (OptimizerParameters,
+                                                ValidOptimizerNames)
+from crystal_diffusion.models.position_diffusion_lightning_model import (
+    PositionDiffusionLightningModel, PositionDiffusionParameters)
+from crystal_diffusion.models.score_network import MLPScoreNetworkParameters
+from crystal_diffusion.samplers.variance_sampler import NoiseParameters
 
 logger = logging.getLogger(__name__)
 
 
-def load_model(hyper_params):  # pragma: no cover
-    """Instantiate a model.
+def load_diffusion_model(hyper_params: Dict[AnyStr, Any]) -> PositionDiffusionLightningModel:
+    """Load a position diffusion model from the hyperparameters.
 
     Args:
-        hyper_params (dict): hyper parameters from the config file
+        hyper_params: dictionary of hyperparameters loaded from a config file
 
     Returns:
-        model (obj): A neural network model object.
+        Diffusion model randomly initialized
     """
-    architecture = hyper_params['architecture']
-    # __TODO__ fix architecture list
-    if architecture == 'simple_mlp':
-        model_class = MLPScoreNetwork
-    else:
-        raise ValueError('architecture {} not supported'.format(architecture))
-    logger.info('selected architecture: {}'.format(architecture))
+    score_network_parameters = MLPScoreNetworkParameters(
+        number_of_atoms=hyper_params['data']['max_atom'],
+        **hyper_params['model']['score_network']
+    )
+    score_network_parameters.spatial_dimension = hyper_params.get('spatial_dimension', 3)
 
-    model = model_class(hyper_params)
+    hyper_params['optimizer']['name'] = ValidOptimizerNames(hyper_params['optimizer']['name'])
+
+    optimizer_parameters = OptimizerParameters(
+        **hyper_params['optimizer']
+    )
+
+    noise_parameters = NoiseParameters(**hyper_params['model']['noise'])
+
+    diffusion_params = PositionDiffusionParameters(
+        score_network_parameters=score_network_parameters,
+        optimizer_parameters=optimizer_parameters,
+        noise_parameters=noise_parameters,
+    )
+
+    model = PositionDiffusionLightningModel(diffusion_params)
     logger.info('model info:\n' + str(model) + '\n')
 
     return model
---
+++ crystal_diffusion/train_diffusion.py
@@ -0,0 +1,248 @@
+"""Entry point to train a diffusion model."""
+import argparse
+import glob
+import logging
+import os
+import shutil
+import sys
+
+import orion
+import pytorch_lightning as pl
+import yaml
+from crystal_diffusion.data.diffusion.data_loader import (
+    LammpsForDiffusionDataModule, LammpsLoaderParameters)
+from crystal_diffusion.models.model_loader import load_diffusion_model
+from crystal_diffusion.utils.file_utils import rsync_folder
+from crystal_diffusion.utils.hp_utils import check_and_log_hp
+from crystal_diffusion.utils.logging_utils import LoggerWriter, log_exp_details
+from crystal_diffusion.utils.reproducibility_utils import set_seed
+from orion.client import report_results
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+from yaml import load
+
+logger = logging.getLogger(__name__)
+
+BEST_MODEL_NAME = 'best_model'
+LAST_MODEL_NAME = 'last_model'
+
+
+def main():
+    """Create and train a diffusion model: main entry point of the program.
+
+    Note:
+        This main.py file is meant to be called using the cli,
+        see the `examples/local/run_diffusion.sh` file to see how to use it.
+
+    """
+    parser = argparse.ArgumentParser()
+    # __TODO__ check you need all the following CLI parameters
+    parser.add_argument('--log', help='log to this file (in addition to stdout/err)')
+    parser.add_argument('--config',
+                        help='config file with generic hyper-parameters,  such as optimizer, '
+                             'batch_size, ... -  in yaml format')
+    parser.add_argument('--data', help='path to a LAMMPS data set', required=True)
+    parser.add_argument('--processed_datadir', help='path to the processed data directory', required=True)
+    parser.add_argument('--dataset_working_dir', help='path to the Datasets working directory. Defaults to None',
+                        default=None)
+    parser.add_argument('--tmp-folder',
+                        help='will use this folder as working folder - it will copy the input data '
+                             'here, generate results here, and then copy them back to the output '
+                             'folder')  # TODO possibly remove this
+    parser.add_argument('--output', help='path to outputs - will store files here', required=True)
+    parser.add_argument('--disable-progressbar', action='store_true',
+                        help='will disable the progressbar while going over the mini-batch')
+    parser.add_argument('--start-from-scratch', action='store_true',
+                        help='will not load any existing saved model - even if present')
+    parser.add_argument('--accelerator', help='PL trainer accelerator. Defaults to auto.', default='auto')
+    parser.add_argument('--devices', default=1, help='pytorch-lightning devices kwarg. Defaults to 1.')
+    parser.add_argument('--debug', action='store_true')  # TODO not used yet
+    args = parser.parse_args()
+
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+    if os.path.exists(args.output) and args.start_from_scratch:
+        logger.info('Starting from scratch, removing any previous experiments.')
+        shutil.rmtree(args.output)
+
+    if os.path.exists(args.output):
+        logger.info("Previous experiment found, resuming from checkpoint")
+    else:
+        os.makedirs(args.output)
+
+    if args.tmp_folder is not None:
+        # TODO data rsync to tmp_folder
+        output_dir = os.path.join(args.tmp_folder, 'output')
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+    else:
+        output_dir = args.output
+
+    # will log to a file if provided (useful for orion on cluster)
+    if args.log is not None:
+        handler = logging.handlers.WatchedFileHandler(args.log)
+        formatter = logging.Formatter(logging.BASIC_FORMAT)
+        handler.setFormatter(formatter)
+        root = logging.getLogger()
+        root.setLevel(logging.INFO)
+        root.addHandler(handler)
+
+    # to intercept any print statement:
+    sys.stdout = LoggerWriter(logger.info)
+    sys.stderr = LoggerWriter(logger.warning)
+
+    if args.config is not None:
+        with open(args.config, 'r') as stream:
+            hyper_params = load(stream, Loader=yaml.FullLoader)
+    else:
+        hyper_params = {}
+
+    run(args, output_dir, hyper_params)
+
+    if args.tmp_folder is not None:
+        rsync_folder(output_dir + os.path.sep, args.output)
+
+
+def run(args, output_dir, hyper_params):
+    """Create and run the dataloaders, training loops, etc.
+
+    Args:
+        args (object): arguments passed from the cli
+        output_dir (str): path to output folder
+        hyper_params (dict): hyper parameters from the config file
+    """
+    # __TODO__ change the hparam that are used from the training algorithm
+    # (and NOT the model - these will be specified in the model itself)
+    logger.info('List of hyper-parameters:')
+    check_and_log_hp(
+        ['model', 'data', 'exp_name', 'max_epoch', 'optimizer', 'seed',
+         'early_stopping'],
+        hyper_params)
+
+    if hyper_params["seed"] is not None:
+        set_seed(hyper_params["seed"])
+
+    log_exp_details(os.path.realpath(__file__), args)
+
+    data_params = LammpsLoaderParameters(**hyper_params['data'])
+
+    datamodule = LammpsForDiffusionDataModule(
+        lammps_run_dir=args.data,
+        processed_dataset_dir=args.processed_datadir,
+        hyper_params=data_params,
+        working_cache_dir=args.dataset_working_dir,
+    )
+
+    model = load_diffusion_model(hyper_params)
+
+    train(model=model, datamodule=datamodule, output=output_dir, hyper_params=hyper_params,
+          use_progress_bar=not args.disable_progressbar, accelerator=args.accelerator, devices=args.devices)
+
+    # clean up the data cache to save disk space
+    datamodule.clean_up()
+
+
+def train(**kwargs):  # pragma: no cover
+    """Training loop wrapper. Used to catch exception if Orion is being used."""
+    try:
+        best_dev_metric = train_impl(**kwargs)
+    except RuntimeError as err:
+        if orion.client.cli.IS_ORION_ON and 'CUDA out of memory' in str(err):
+            logger.error(err)
+            logger.error('model was out of memory - assigning a bad score to tell Orion to avoid'
+                         'too big model')
+            best_dev_metric = -999
+        else:
+            raise err
+
+    report_results([dict(
+        name='dev_metric',
+        type='objective',
+        # note the minus - cause orion is always trying to minimize (cit. from the guide)
+        value=-float(best_dev_metric))])
+
+
+def train_impl(model, datamodule, output, hyper_params, use_progress_bar, accelerator=None, devices=None
+               ):  # pragma: no cover
+    """Train a model: main training loop implementation.
+
+    Args:
+        model (obj): The neural network model object.
+        datamodule (obj): lightning data module that will instantiate data loaders.
+        output (str): Output directory.
+        hyper_params (dict): Dict containing hyper-parameters.
+        use_progress_bar (bool): Use tqdm progress bar (can be disabled when logging).
+        accelerator: PL trainer accelerator
+        devices: PL devices to use
+    """
+    check_and_log_hp(['max_epoch'], hyper_params)
+
+    best_model_path = os.path.join(output, BEST_MODEL_NAME)
+    best_checkpoint_callback = ModelCheckpoint(
+        dirpath=best_model_path,
+        filename='model',
+        save_top_k=1,
+        verbose=use_progress_bar,
+        monitor="val_loss",
+        mode="max",
+        every_n_epochs=1,
+    )
+
+    last_model_path = os.path.join(output, LAST_MODEL_NAME)
+    last_checkpoint_callback = ModelCheckpoint(
+        dirpath=last_model_path,
+        filename='model',
+        verbose=use_progress_bar,
+        every_n_epochs=1,
+    )
+
+    # TODO pl Trainer does not use the kwarg resume_from_checkpoint now - check about resume training works now
+    # resume_from_checkpoint = handle_previous_models(output, last_model_path, best_model_path)
+
+
+    early_stopping_params = hyper_params['early_stopping']
+    check_and_log_hp(['metric', 'mode', 'patience'], hyper_params['early_stopping'])
+    early_stopping = EarlyStopping(
+        early_stopping_params['metric'],
+        mode=early_stopping_params['mode'],
+        patience=early_stopping_params['patience'],
+        verbose=use_progress_bar)
+
+    logger = pl.loggers.TensorBoardLogger(
+        save_dir=output,
+        default_hp_metric=False,
+        version=0,  # Necessary to resume tensorboard logging
+    )
+
+    trainer = pl.Trainer(
+        callbacks=[early_stopping, best_checkpoint_callback, last_checkpoint_callback],
+        max_epochs=hyper_params['max_epoch'],
+        # resume_from_checkpoint=resume_from_checkpoint,
+        accelerator=accelerator,
+        devices=devices,
+        logger=logger,
+    )
+
+    trainer.fit(model, datamodule=datamodule)
+
+    # Log the best result and associated hyper parameters
+    best_dev_result = float(early_stopping.best_score.cpu().numpy())
+    logger.log_hyperparams(hyper_params, metrics={'best_dev_metric': best_dev_result})
+
+    return best_dev_result
+
+
+def handle_previous_models(output, last_model_path, best_model_path):
+    """Move the previous models in a new timestamp folder."""
+    last_models = glob.glob(last_model_path + os.sep + '*')
+
+    if len(last_models) >= 1:
+        resume_from_checkpoint = sorted(last_models)[-1]
+        logger.info(f'models found - resuming from {resume_from_checkpoint}')
+    else:
+        logger.info('no model found - starting training from scratch')
+        resume_from_checkpoint = None
+    return resume_from_checkpoint
+
+
+if __name__ == '__main__':
+    main()
