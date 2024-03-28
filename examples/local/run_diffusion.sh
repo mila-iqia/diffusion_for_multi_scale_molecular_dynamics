13fc5439f499a5b0276ea6af9f3f5edb3d999561
Simon Blackburn <simon.blackburn@mila.quebec>
2024-03-28 14:19:06 -0400

    end2end diffusion model training

---
+++ examples/local/run_diffusion.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+LOG=debug.log
+CONFIG=config_diffusion.yaml
+DATA_DIR=../../data/si_diffusion_v1
+PROCESSED_DATA=${DATA_DIR}/processed
+DATA_WORK_DIR=./tmp_work_dir/
+OUTPUT=debug
+
+python ../../crystal_diffusion/train_diffusion.py \
+    --log $LOG \
+    --config $CONFIG \
+    --data $DATA_DIR \
+    --processed_datadir $PROCESSED_DATA \
+    --dataset_working_dir $DATA_WORK_DIR \
+    --output $OUTPUT
