46494dcf6d4dbbcce74081422b8e851a1b9b78ac
Simon Blackburn <simon.blackburn@mila.quebec>
2024-04-04 09:59:10 -0400

    line length error

--- tests/oracle/test_lammps.py
+++ tests/oracle/test_lammps.py
@@ -19,7 +19,8 @@ def high_symmetry_positions():
 # do not run on github because no lammps
 @pytest.mark.not_on_github
 def test_high_symmetry(high_symmetry_positions, high_symmetry_lattice):
-    energy, forces = get_energy_and_forces_from_lammps(high_symmetry_positions, high_symmetry_lattice, atom_types=np.array([1, 1]))
+    energy, forces = get_energy_and_forces_from_lammps(high_symmetry_positions, high_symmetry_lattice,
+                                                       atom_types=np.array([1, 1]))
     for x in ['x', 'y', 'z']:
         assert np.allclose(forces[f'f{x}'], [0, 0])
     assert energy < 0
@@ -33,7 +34,8 @@ def low_symmetry_positions():
 
 @pytest.mark.not_on_github
 def test_low_symmetry(low_symmetry_positions, high_symmetry_lattice):
-    energy, forces = get_energy_and_forces_from_lammps(low_symmetry_positions, high_symmetry_lattice, atom_types=np.array([1, 1]))
+    energy, forces = get_energy_and_forces_from_lammps(low_symmetry_positions, high_symmetry_lattice,
+                                                       atom_types=np.array([1, 1]))
     for x in ['x', 'y', 'z']:
         assert not np.allclose(forces[f'f{x}'], [0, 0])
     assert energy < 0
