aa2feca5b5db49df6ff2ec6079b3ab746045f9b9
Simon Blackburn <simon.blackburn@mila.quebec>
2024-04-16 12:43:42 -0400

    code review fixes

--- examples/local_orion/diffusion/run_orion.sh
+++ examples/local_orion/diffusion/run_orion.sh
@@ -1,14 +1,14 @@
 export ORION_DB_ADDRESS='orion_db.pkl'
 export ORION_DB_TYPE='pickleddb'
 
+ROOT_DIR=../../../
 CONFIG=config_diffusion.yaml
-DATA_DIR=../../data/si_diffusion_small
+DATA_DIR=${ROOT_DIR}/data/si_diffusion_small
 PROCESSED_DATA=${DATA_DIR}/processed
 DATA_WORK_DIR=./tmp_work_dir/
-OUTPUT=output
 
 orion -v hunt --config orion_config.yaml \
-    python ../../crystal_diffusion/train_diffusion.py \
+    python ${ROOT_DIR}/crystal_diffusion/train_diffusion.py \
     --config $CONFIG \
     --data $DATA_DIR \
     --processed_datadir $PROCESSED_DATA \
