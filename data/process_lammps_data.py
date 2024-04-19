5b9ad2ab62693334599f87ca0f644b7e93a8ec7e
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-04-19 16:32:48 -0400

    Make sure max atom is an int.

--- data/process_lammps_data.py
+++ data/process_lammps_data.py
@@ -17,7 +17,7 @@ def main():
 
     lammps_run_dir = args.data
     processed_dataset_dir = args.processed_datadir
-    data_params = LammpsLoaderParameters(batch_size=128, num_workers=0, max_atom=args.max_atom)
+    data_params = LammpsLoaderParameters(batch_size=128, num_workers=0, max_atom=int(args.max_atom))
 
     with tempfile.TemporaryDirectory() as tmp_work_dir:
         data_module = LammpsForDiffusionDataModule(lammps_run_dir=lammps_run_dir,
