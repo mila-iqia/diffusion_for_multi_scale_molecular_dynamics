981fd96d7a38350e6f76cd38c47fdbe96467d9b3
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-04-23 14:52:07 -0400

    Remove complex test case.

--- tests/data/test_parse_lammps_output.py
+++ tests/data/test_parse_lammps_output.py
@@ -22,25 +22,33 @@ def generate_fake_yaml(filename, documents, multiple_docs=True):
             yaml.dump(documents, yaml_file)
 
 
-@pytest.fixture
-def fake_lammps_yaml(tmpdir):
+@pytest.fixture()
+def fake_yaml_content():
     # fake LAMMPS output file with 4 MD steps in 1D for 3 atoms
-    yaml_content = [
-        {'keywords': ['id', 'type', 'x', 'fx'],
-         'data': [[0, 1, 0.1, 0.01], [1, 2, 0.2, 0.02], [2, 1, 0.3, 0.03]],
-         'box': [[0, 0.6], [0, 1.6], [0, 2.6]]},
-        {'keywords': ['id', 'type', 'x', 'fx'],
-         'data': [[0, 1, 1.1, 1.01], [1, 2, 1.2, 1.02], [2, 1, 1.3, 1.03]],
-         'box': [[0, 0.6], [0, 1.6], [0, 2.6]]},
-        {'keywords': ['id', 'type', 'x', 'fx'],
-         'data': [[0, 1, 2.1, 2.01], [1, 2, 2.2, 2.02], [2, 1, 2.3, 2.03]],
-         'box': [[0, 0.6], [0, 1.6], [0, 2.6]]},
-        {'keywords': ['id', 'type', 'x', 'fx'],
-         'data': [[0, 1, 3.1, 3.01], [1, 2, 3.2, 3.02], [2, 1, 3.3, 3.03]],
-         'box': [[0, 0.6], [0, 1.6], [0, 2.6]]},
-    ]
+    np.random.seed(23423)
+    box = [[0, 0.6], [0, 1.6], [0, 2.6]]
+    keywords = ['id', 'type', 'x', 'y', 'z', 'fx', 'fy', 'fz']
+
+    number_of_documents = 4
+    list_atom_types = [1, 2, 1]
+
+    yaml_content = []
+    for doc_idx in range(number_of_documents):
+
+        data = []
+        for id, atom_type in enumerate(list_atom_types):
+            row = [id, atom_type] + list(np.random.rand(6))
+            data.append(row)
+
+        doc = dict(keywords=keywords, box=box, data=data)
+        yaml_content.append(doc)
+    return yaml_content
+
+
+@pytest.fixture
+def fake_lammps_yaml(tmpdir, fake_yaml_content):
     file = os.path.join(tmpdir, 'fake_lammps_dump.yaml')
-    generate_fake_yaml(file, yaml_content)
+    generate_fake_yaml(file, fake_yaml_content)
     return file
 
 
@@ -95,20 +103,6 @@ def test_parse_lammps_outputs(fake_lammps_yaml, fake_thermo_yaml, expected_proce
 
     pd.testing.assert_frame_equal(expected_processed_thermo_dataframe, df[expected_processed_thermo_dataframe.columns])
 
-    for i, v in enumerate(['id', 'type', 'x', 'fx', 'box']):
-        assert v in df.keys()
-        for x in range(4):
-            if v == 'id':
-                assert np.array_equal(df[v][x], [0, 1, 2])
-            elif v == 'type':
-                assert np.array_equal(df[v][x], [1, 2, 1])
-            elif v == 'x':
-                assert np.allclose(df[v][x], [x + 0.1 * y for y in range(1, 4)])
-            elif v == 'fx':
-                assert np.allclose(df[v][x], [x + 0.01 * y for y in range(1, 4)])
-            else:  # v == 'box'
-                assert np.allclose(df[v][x], [x + 0.6 for x in range(3)])
-
 
 @pytest.mark.parametrize("pressure", [True, False])
 @pytest.mark.parametrize("temperature", [True, False])
@@ -128,7 +122,7 @@ def spatial_dimension():
 
 @pytest.fixture()
 def number_of_atoms():
-    return 8
+    return 64
 
 
 @pytest.fixture()
@@ -160,7 +154,9 @@ def expected_lammps_dump_dataframe(configurations):
 
 
 def test_parse_lammps_dump(lammps_dump_path, expected_lammps_dump_dataframe):
+
     data_dict = parse_lammps_dump(lammps_dump_path)
+
     computed_lammps_dump_dataframe = pd.DataFrame(data_dict)
 
     assert set(computed_lammps_dump_dataframe.columns) == set(expected_lammps_dump_dataframe)
