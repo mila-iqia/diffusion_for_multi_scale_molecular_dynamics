463d493afac3eeae4c2a24aca728d00c81d330f7
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-05-22 15:09:38 -0400

    Equivariant diffusion mace model.

---
+++ crystal_diffusion/models/diffusion_mace.py
@@ -0,0 +1,299 @@
+from typing import AnyStr, Callable, Dict, List, Optional, Type, Union
+
+import numpy as np
+import torch
+from e3nn import o3
+from mace.modules import (AtomicEnergiesBlock, EquivariantProductBasisBlock,
+                          InteractionBlock, LinearNodeEmbeddingBlock,
+                          LinearReadoutBlock, NonLinearReadoutBlock,
+                          RadialEmbeddingBlock)
+from mace.modules.utils import (get_edge_vectors_and_lengths, get_outputs,
+                                get_symmetric_displacement)
+from mace.tools.scatter import scatter_sum
+from torch_geometric.data import Data
+
+from crystal_diffusion.models.mace_utils import get_adj_matrix
+from crystal_diffusion.namespace import (NOISY_CARTESIAN_POSITIONS, TIME,
+                                         UNIT_CELL)
+
+
+def input_to_diffusion_mace(batch: Dict[AnyStr, torch.Tensor], radial_cutoff: float) -> Data:
+    """Convert score network input to Diffusion MACE input.
+
+    Args:
+        batch: score network input dictionary
+        radial_cutoff : largest distance between neighbors.
+
+    Returns:
+        pytorch-geometric graph data compatible with MACE forward
+    """
+    cartesian_positions = batch[NOISY_CARTESIAN_POSITIONS]
+    batch_size, n_atom_per_graph, spatial_dimension = cartesian_positions.shape
+    device = cartesian_positions.device
+
+    basis_vectors = batch[UNIT_CELL]  # batch, spatial_dimension, spatial_dimension
+
+    adj_matrix, shift_matrix, batch_tensor = get_adj_matrix(positions=cartesian_positions,
+                                                            basis_vectors=basis_vectors,
+                                                            radial_cutoff=radial_cutoff)
+
+    # The node attributes will be the diffusion time, which is constant for each structure in the batch.
+    times = batch[TIME]  # [batch_size, 1]
+    node_attrs = times.repeat_interleave(n_atom_per_graph, dim=0)  # [flat_batch_size, 1]
+
+    # [batchsize * natoms, spatial dimension]
+    flat_cartesian_positions = cartesian_positions.view(-1, spatial_dimension)
+
+    # pointer tensor that yields the first node index for each batch - this is a fixed tensor in our case
+    ptr = torch.arange(0, n_atom_per_graph * batch_size + 1, step=n_atom_per_graph)  # 0, natoms, 2 * natoms, ...
+
+    flat_basis_vectors = basis_vectors.view(-1, spatial_dimension)  # batch * spatial_dimension, spatial_dimension
+    # create the pytorch-geometric graph
+    graph_data = Data(edge_index=adj_matrix,
+                      node_attrs=node_attrs.to(device),
+                      positions=flat_cartesian_positions,
+                      ptr=ptr.to(device),
+                      batch=batch_tensor.to(device),
+                      shifts=shift_matrix,
+                      cell=flat_basis_vectors
+                      )
+    return graph_data
+
+
+class DiffusionMACE(torch.nn.Module):
+    """This architecture is inspired by MACE, but modified to take in diffusion time as a scalar."""
+    def __init__(
+        self,
+        r_max: float,
+        num_bessel: int,
+        num_polynomial_cutoff: int,
+        max_ell: int,
+        interaction_cls: Type[InteractionBlock],
+        interaction_cls_first: Type[InteractionBlock],
+        num_interactions: int,
+        num_elements: int,
+        hidden_irreps: o3.Irreps,
+        MLP_irreps: o3.Irreps,
+        atomic_energies: np.ndarray,
+        avg_num_neighbors: float,
+        atomic_numbers: List[int],
+        correlation: Union[int, List[int]],
+        gate: Optional[Callable],
+        radial_MLP: List[int],
+        radial_type: Optional[str] = "bessel",
+    ):
+        """Init method."""
+        super().__init__()
+        self.register_buffer(
+            "atomic_numbers", torch.tensor(atomic_numbers, dtype=torch.int64)
+        )
+        self.register_buffer(
+            "r_max", torch.tensor(r_max, dtype=torch.get_default_dtype())
+        )
+        self.register_buffer(
+            "num_interactions", torch.tensor(num_interactions, dtype=torch.int64)
+        )
+        if isinstance(correlation, int):
+            correlation = [correlation] * num_interactions
+        # Embedding
+
+        # define the "0e" representation as a constant to avoid "magic numbers" below.
+        scalar_irrep = o3.Irrep(0, 1)
+
+        # we assume a single 'scalar' diffusion time-like  input.
+        number_of_node_scalar_dimensions = 1
+
+        node_attr_irreps = o3.Irreps([(number_of_node_scalar_dimensions, scalar_irrep)])
+        number_of_hidden_scalar_dimensions = hidden_irreps.count(scalar_irrep)
+
+        # The node features will be only '0e' scalars, with a number of channels identical with the hidden irreps
+        node_feats_irreps = o3.Irreps([(number_of_hidden_scalar_dimensions, scalar_irrep)])
+        self.node_embedding = LinearNodeEmbeddingBlock(
+            irreps_in=node_attr_irreps, irreps_out=node_feats_irreps
+        )
+        self.radial_embedding = RadialEmbeddingBlock(
+            r_max=r_max,
+            num_bessel=num_bessel,
+            num_polynomial_cutoff=num_polynomial_cutoff,
+            radial_type=radial_type,
+        )
+        edge_feats_irreps = o3.Irreps([(self.radial_embedding.out_dim, scalar_irrep)])
+
+        sh_irreps = o3.Irreps.spherical_harmonics(max_ell)
+        interaction_irreps = (sh_irreps * number_of_hidden_scalar_dimensions).sort()[0].simplify()
+        self.spherical_harmonics = o3.SphericalHarmonics(
+            sh_irreps, normalize=True, normalization="component"
+        )
+        # Interactions and readout
+        self.atomic_energies_fn = AtomicEnergiesBlock(atomic_energies)
+
+        inter = interaction_cls_first(
+            node_attrs_irreps=node_attr_irreps,
+            node_feats_irreps=node_feats_irreps,
+            edge_attrs_irreps=sh_irreps,
+            edge_feats_irreps=edge_feats_irreps,
+            target_irreps=interaction_irreps,
+            hidden_irreps=hidden_irreps,
+            avg_num_neighbors=avg_num_neighbors,
+            radial_MLP=radial_MLP,
+        )
+        self.interactions = torch.nn.ModuleList([inter])
+
+        # Use the appropriate self connection at the first layer for proper E0
+        use_sc_first = False
+        if "Residual" in str(interaction_cls_first):
+            use_sc_first = True
+
+        node_feats_irreps_out = inter.target_irreps
+        prod = EquivariantProductBasisBlock(
+            node_feats_irreps=node_feats_irreps_out,
+            target_irreps=hidden_irreps,
+            correlation=correlation[0],
+            num_elements=num_elements,
+            use_sc=use_sc_first,
+        )
+        self.products = torch.nn.ModuleList([prod])
+
+        self.readouts = torch.nn.ModuleList()
+        self.readouts.append(LinearReadoutBlock(hidden_irreps))
+
+        for i in range(num_interactions - 1):
+            if i == num_interactions - 2:
+                hidden_irreps_out = str(
+                    hidden_irreps[0]
+                )  # Select only scalars for last layer
+            else:
+                hidden_irreps_out = hidden_irreps
+            inter = interaction_cls(
+                node_attrs_irreps=node_attr_irreps,
+                node_feats_irreps=hidden_irreps,
+                edge_attrs_irreps=sh_irreps,
+                edge_feats_irreps=edge_feats_irreps,
+                target_irreps=interaction_irreps,
+                hidden_irreps=hidden_irreps_out,
+                avg_num_neighbors=avg_num_neighbors,
+                radial_MLP=radial_MLP,
+            )
+            self.interactions.append(inter)
+            prod = EquivariantProductBasisBlock(
+                node_feats_irreps=interaction_irreps,
+                target_irreps=hidden_irreps_out,
+                correlation=correlation[i + 1],
+                num_elements=num_elements,
+                use_sc=True,
+            )
+            self.products.append(prod)
+            if i == num_interactions - 2:
+                self.readouts.append(
+                    NonLinearReadoutBlock(hidden_irreps_out, MLP_irreps, gate)
+                )
+            else:
+                self.readouts.append(LinearReadoutBlock(hidden_irreps))
+
+    def forward(
+        self,
+        data: Dict[str, torch.Tensor],
+        training: bool = False,
+        compute_force: bool = True,
+        compute_virials: bool = False,
+        compute_stress: bool = False,
+        compute_displacement: bool = False,
+    ) -> Dict[str, Optional[torch.Tensor]]:
+        """Forward method."""
+        # Setup
+        data["node_attrs"].requires_grad_(True)
+        data["positions"].requires_grad_(True)
+        num_graphs = data["ptr"].numel() - 1
+        displacement = torch.zeros(
+            (num_graphs, 3, 3),
+            dtype=data["positions"].dtype,
+            device=data["positions"].device,
+        )
+        if compute_virials or compute_stress or compute_displacement:
+            (
+                data["positions"],
+                data["shifts"],
+                displacement,
+            ) = get_symmetric_displacement(
+                positions=data["positions"],
+                unit_shifts=data["unit_shifts"],
+                cell=data["cell"],
+                edge_index=data["edge_index"],
+                num_graphs=num_graphs,
+                batch=data["batch"],
+            )
+
+        # Atomic energies
+        node_e0 = self.atomic_energies_fn(data["node_attrs"])
+        e0 = scatter_sum(
+            src=node_e0, index=data["batch"], dim=-1, dim_size=num_graphs
+        )  # [n_graphs,]
+
+        # Embeddings
+        node_feats = self.node_embedding(data["node_attrs"])
+        vectors, lengths = get_edge_vectors_and_lengths(
+            positions=data["positions"],
+            edge_index=data["edge_index"],
+            shifts=data["shifts"],
+        )
+        edge_attrs = self.spherical_harmonics(vectors)
+        edge_feats = self.radial_embedding(lengths)
+
+        # Interactions
+        energies = [e0]
+        node_energies_list = [node_e0]
+        node_feats_list = []
+        for interaction, product, readout in zip(
+            self.interactions, self.products, self.readouts
+        ):
+            node_feats, sc = interaction(
+                node_attrs=data["node_attrs"],
+                node_feats=node_feats,
+                edge_attrs=edge_attrs,
+                edge_feats=edge_feats,
+                edge_index=data["edge_index"],
+            )
+            node_feats = product(
+                node_feats=node_feats,
+                sc=sc,
+                node_attrs=data["node_attrs"],
+            )
+            node_feats_list.append(node_feats)
+            node_energies = readout(node_feats).squeeze(-1)  # [n_nodes, ]
+            energy = scatter_sum(
+                src=node_energies, index=data["batch"], dim=-1, dim_size=num_graphs
+            )  # [n_graphs,]
+            energies.append(energy)
+            node_energies_list.append(node_energies)
+
+        # Concatenate node features
+        node_feats_out = torch.cat(node_feats_list, dim=-1)
+
+        # Sum over energy contributions
+        contributions = torch.stack(energies, dim=-1)
+        total_energy = torch.sum(contributions, dim=-1)  # [n_graphs, ]
+        node_energy_contributions = torch.stack(node_energies_list, dim=-1)
+        node_energy = torch.sum(node_energy_contributions, dim=-1)  # [n_nodes, ]
+
+        # Outputs
+        forces, virials, stress = get_outputs(
+            energy=total_energy,
+            positions=data["positions"],
+            displacement=displacement,
+            cell=data["cell"],
+            training=training,
+            compute_force=compute_force,
+            compute_virials=compute_virials,
+            compute_stress=compute_stress,
+        )
+
+        return {
+            "energy": total_energy,
+            "node_energy": node_energy,
+            "contributions": contributions,
+            "forces": forces,
+            "virials": virials,
+            "stress": stress,
+            "displacement": displacement,
+            "node_feats": node_feats_out,
+        }
---
+++ tests/models/test_diffusion_mace.py
@@ -0,0 +1,224 @@
+from typing import Tuple
+
+import numpy as np
+import pytest
+import torch
+from e3nn import o3
+from mace.modules import gate_dict, interaction_classes
+
+from crystal_diffusion.models.diffusion_mace import (DiffusionMACE,
+                                                     input_to_diffusion_mace)
+from crystal_diffusion.namespace import (NOISY_CARTESIAN_POSITIONS,
+                                         NOISY_RELATIVE_COORDINATES, TIME,
+                                         UNIT_CELL)
+from crystal_diffusion.utils.basis_transformations import (
+    get_positions_from_coordinates, get_reciprocal_basis_vectors,
+    get_relative_coordinates_from_cartesian_positions,
+    map_relative_coordinates_to_unit_cell)
+
+
+def get_new_positions_and_coordinates(transformed_cartesian_positions: torch.Tensor,
+                                      basis_vectors: torch.Tensor,
+                                      reciprocal_basis_vectors: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    """Get new positions and coordinates.
+
+    Args:
+        transformed_cartesian_positions : cartesian positions after some operation. These may be outside the unit cell.
+        basis_vectors : vectors that define the unit cell
+        reciprocal_basis_vectors : vectors that define the reciprocal unit cell
+
+    Returns:
+        new_cartesian_positions, new_relative_coordinates
+    """
+    raw_relative_coordinates = get_relative_coordinates_from_cartesian_positions(transformed_cartesian_positions,
+                                                                                 reciprocal_basis_vectors)
+
+    new_relative_coordinates = map_relative_coordinates_to_unit_cell(raw_relative_coordinates)
+
+    new_cartesian_positions = get_positions_from_coordinates(new_relative_coordinates, basis_vectors)
+
+    return new_cartesian_positions, new_relative_coordinates
+
+
+class TestDiffusionMace:
+
+    @pytest.fixture(scope="class", autouse=True)
+    def set_seed(self):
+        """Set the random seed."""
+        torch.manual_seed(234233)
+
+    @pytest.fixture(scope="class", autouse=True)
+    def set_torch_default_type(self):
+        torch.set_default_dtype(torch.float64)
+
+    @pytest.fixture(scope='class')
+    def batch_size(self):
+        return 4
+
+    @pytest.fixture(scope='class')
+    def number_of_atoms(self):
+        return 8
+
+    @pytest.fixture(scope='class')
+    def spatial_dimension(self):
+        return 3
+
+    @pytest.fixture(scope='class')
+    def basis_vectors(self, batch_size, spatial_dimension):
+        # orthogonal boxes with dimensions between 5 and 10.
+        orthogonal_boxes = torch.stack([torch.diag(5. + 5. * torch.rand(spatial_dimension))
+                                        for _ in range(batch_size)])
+        # add a bit of noise to make the vectors not quite orthogonal
+        basis_vectors = orthogonal_boxes + 0.1 * torch.randn(batch_size, spatial_dimension, spatial_dimension)
+        return basis_vectors
+
+    @pytest.fixture(scope='class')
+    def reciprocal_basis_vectors(self, basis_vectors):
+        return get_reciprocal_basis_vectors(basis_vectors)
+
+    @pytest.fixture(scope='class')
+    def relative_coordinates(self, batch_size, number_of_atoms, spatial_dimension):
+        relative_coordinates = torch.rand(batch_size, number_of_atoms, spatial_dimension)
+        return relative_coordinates
+
+    @pytest.fixture(scope='class')
+    def cartesian_positions(self, relative_coordinates, basis_vectors):
+        return get_positions_from_coordinates(relative_coordinates, basis_vectors)
+
+    @pytest.fixture(scope='class')
+    def times(self, batch_size):
+        return torch.rand(batch_size, 1)
+
+    @pytest.fixture(scope='class')
+    def batch(self, relative_coordinates, cartesian_positions, basis_vectors, times):
+        batch = {NOISY_RELATIVE_COORDINATES: relative_coordinates,
+                 NOISY_CARTESIAN_POSITIONS: cartesian_positions,
+                 TIME: times,
+                 UNIT_CELL: basis_vectors}
+        return batch
+
+    @pytest.fixture(scope='class')
+    def cartesian_rotations(self, batch_size):
+        return o3.rand_matrix(batch_size)
+
+    @pytest.fixture(scope='class')
+    def cartesian_translations(self, batch_size, number_of_atoms, spatial_dimension, basis_vectors):
+        batch_relative_coordinates_translations = torch.rand(batch_size, spatial_dimension)
+
+        batch_cartesian_translations = []
+        for t, cell in zip(batch_relative_coordinates_translations, basis_vectors):
+            batch_cartesian_translations.append(t @ cell)
+
+        batch_cartesian_translations = torch.stack(batch_cartesian_translations)
+
+        cartesian_translations = torch.repeat_interleave(batch_cartesian_translations.unsqueeze(1),
+                                                         number_of_atoms, dim=1)
+        return cartesian_translations
+
+    @pytest.fixture()
+    def r_max(self):
+        return 3.0
+
+    @pytest.fixture()
+    def hyperparameters(self, r_max):
+
+        hps = dict(r_max=r_max,
+                   num_bessel=8,
+                   num_polynomial_cutoff=5,
+                   max_ell=2,
+                   num_elements=1,
+                   atomic_numbers=[14],
+                   interaction_cls=interaction_classes["RealAgnosticResidualInteractionBlock"],
+                   interaction_cls_first=interaction_classes["RealAgnosticInteractionBlock"],
+                   num_interactions=2,
+                   hidden_irreps=o3.Irreps("128x0e + 128x1o"),
+                   MLP_irreps=o3.Irreps("16x0e"),
+                   atomic_energies=np.array([0.]),
+                   avg_num_neighbors=1,
+                   correlation=3,
+                   gate=gate_dict["silu"],
+                   radial_MLP=[64, 64, 64],
+                   radial_type="bessel")
+        return hps
+
+    @pytest.fixture()
+    def diffusion_mace(self, hyperparameters):
+        diffusion_mace = DiffusionMACE(**hyperparameters)
+        diffusion_mace.eval()
+        return diffusion_mace
+
+    @pytest.fixture()
+    def graph_input(self, batch, r_max):
+        return input_to_diffusion_mace(batch, radial_cutoff=r_max)
+
+    @pytest.fixture()
+    def cartesian_scores(self, graph_input, diffusion_mace, batch_size, number_of_atoms, spatial_dimension):
+        flat_cartesian_scores = diffusion_mace(graph_input, training=False, compute_force=True)['forces']
+        return flat_cartesian_scores.reshape(batch_size, number_of_atoms, spatial_dimension)
+
+    @pytest.fixture()
+    def translated_graph_input(self, batch, r_max, basis_vectors, reciprocal_basis_vectors, cartesian_translations):
+
+        translated_batch = dict(batch)
+
+        original_cartesian_positions = translated_batch[NOISY_CARTESIAN_POSITIONS]
+        translated_cartesian_positions = original_cartesian_positions + cartesian_translations
+
+        rel_coords = get_relative_coordinates_from_cartesian_positions(translated_cartesian_positions,
+                                                                       reciprocal_basis_vectors)
+        new_relative_coordinates = map_relative_coordinates_to_unit_cell(rel_coords)
+
+        new_cartesian_positions = get_positions_from_coordinates(new_relative_coordinates, basis_vectors)
+
+        translated_batch[NOISY_CARTESIAN_POSITIONS] = new_cartesian_positions
+        translated_batch[NOISY_RELATIVE_COORDINATES] = new_relative_coordinates
+
+        return input_to_diffusion_mace(translated_batch, radial_cutoff=r_max)
+
+    @pytest.fixture()
+    def translated_cartesian_scores(self, diffusion_mace, batch_size, number_of_atoms,
+                                    spatial_dimension, basis_vectors, translated_graph_input):
+        flat_translated_cartesian_scores = diffusion_mace(translated_graph_input,
+                                                          training=False, compute_force=True)['forces']
+        return flat_translated_cartesian_scores.reshape(batch_size, number_of_atoms, spatial_dimension)
+
+    @pytest.fixture()
+    def rotated_graph_input(self, batch, r_max, basis_vectors, reciprocal_basis_vectors, cartesian_rotations):
+        rotated_batch = dict(batch)
+
+        original_cartesian_positions = rotated_batch[NOISY_CARTESIAN_POSITIONS]
+        original_basis_vectors = rotated_batch[UNIT_CELL]
+
+        rotated_cartesian_positions = torch.matmul(original_cartesian_positions,
+                                                   cartesian_rotations.transpose(2, 1))
+
+        rotated_basis_vectors = torch.matmul(original_basis_vectors, cartesian_rotations.transpose(2, 1))
+        rotated_reciprocal_basis_vectors = get_reciprocal_basis_vectors(rotated_basis_vectors)
+
+        rel_coords = get_relative_coordinates_from_cartesian_positions(rotated_cartesian_positions,
+                                                                       rotated_reciprocal_basis_vectors)
+        new_relative_coordinates = map_relative_coordinates_to_unit_cell(rel_coords)
+        new_cartesian_positions = get_positions_from_coordinates(new_relative_coordinates, rotated_basis_vectors)
+
+        rotated_batch[NOISY_CARTESIAN_POSITIONS] = new_cartesian_positions
+        rotated_batch[NOISY_RELATIVE_COORDINATES] = new_relative_coordinates
+        rotated_batch[UNIT_CELL] = rotated_basis_vectors
+
+        return input_to_diffusion_mace(rotated_batch, radial_cutoff=r_max)
+
+    @pytest.fixture()
+    def rotated_cartesian_scores(self, diffusion_mace, batch_size, number_of_atoms,
+                                 spatial_dimension, rotated_graph_input):
+        flat_rotated_cartesian_scores = diffusion_mace(rotated_graph_input,
+                                                       training=False, compute_force=True)['forces']
+        return flat_rotated_cartesian_scores.reshape(batch_size, number_of_atoms, spatial_dimension)
+
+    def test_translation_invariance(self, cartesian_scores, translated_cartesian_scores):
+        torch.testing.assert_close(translated_cartesian_scores, cartesian_scores)
+
+    def test_rotation_equivariance(self, cartesian_scores, rotated_cartesian_scores, cartesian_rotations):
+        vector_irreps = o3.Irreps('1o')
+        d_matrices = vector_irreps.D_from_matrix(cartesian_rotations)
+
+        expected_rotated_cartesian_scores = torch.matmul(cartesian_scores, d_matrices.transpose(2, 1))
+        torch.testing.assert_close(expected_rotated_cartesian_scores, rotated_cartesian_scores)
