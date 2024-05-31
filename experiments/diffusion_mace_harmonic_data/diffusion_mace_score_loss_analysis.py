616a1df0598688bbc11a32fda4459ff80854a9b4
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-05-31 09:21:05 -0400

    More ad hoc experiments.

--- experiments/diffusion_mace_harmonic_data/diffusion_mace_score_loss_analysis.py
+++ experiments/diffusion_mace_harmonic_data/diffusion_mace_score_loss_analysis.py
@@ -21,7 +21,10 @@ from crystal_diffusion.models.position_diffusion_lightning_model import (
 from crystal_diffusion.models.scheduler import (
     ReduceLROnPlateauSchedulerParameters, ValidSchedulerName)
 from crystal_diffusion.models.score_network import (
-    DiffusionMACEScoreNetworkParameters, MLPScoreNetworkParameters)
+    DiffusionMACEScoreNetworkParameters, MACEScoreNetworkParameters,
+    MLPScoreNetworkParameters)
+from crystal_diffusion.models.score_prediction_head import \
+    MaceEquivariantScorePredictionHeadParameters
 from crystal_diffusion.namespace import RELATIVE_COORDINATES
 from crystal_diffusion.samplers.variance_sampler import NoiseParameters
 
@@ -31,40 +34,54 @@ logger = logging.getLogger(__name__)
 plt.style.use(PLOT_STYLE_PATH)
 
 # model = 'mlp'
-model = 'mace'
+# model = 'diffusion_mace'
+model = 'mace_plus_prediction_head'
 run_id = 2
 
 spatial_dimension = 3
 number_of_atoms = 2
 
 dataset_size = 100_000
-batch_size = 1024
+batch_size = 128
+gradient_clip_val = 0.0
 
 spring_constant = 1000.
 
-mace_score_network_parameters = DiffusionMACEScoreNetworkParameters(
-    number_of_atoms=2,
-    r_max=5.0,
-    num_bessel=4,
-    num_polynomial_cutoff=3,
-    max_ell=1,
-    interaction_cls="RealAgnosticResidualInteractionBlock",
-    interaction_cls_first="RealAgnosticInteractionBlock",
-    num_interactions=2,
-    hidden_irreps="16x0e + 16x1o",
-    MLP_irreps="16x0e",
-    avg_num_neighbors=1,
-    correlation=2,
-    gate="silu",
-    radial_MLP=[16, 16, 16],
-    radial_type="bessel"
-)
+common_mace_parameters = dict(number_of_atoms=number_of_atoms,
+                              r_max=5.0,
+                              num_bessel=8,
+                              num_polynomial_cutoff=5,
+                              max_ell=2,
+                              interaction_cls="RealAgnosticResidualInteractionBlock",
+                              interaction_cls_first="RealAgnosticInteractionBlock",
+                              num_interactions=2,
+                              hidden_irreps="32x0e + 32x1o",
+                              MLP_irreps="32x0e",
+                              avg_num_neighbors=1,
+                              correlation=2,
+                              gate="silu",
+                              radial_MLP=[64, 64, 64],
+                              radial_type="gaussian")
+
+
+prediction_head_parameters = MaceEquivariantScorePredictionHeadParameters(time_embedding_irreps="32x0e",
+                                                                          gate="silu",
+                                                                          number_of_layers=3)
+
+mace_score_network_parameters = MACEScoreNetworkParameters(prediction_head_parameters=prediction_head_parameters,
+                                                           **common_mace_parameters)
+
+diffusion_mace_score_network_parameters = DiffusionMACEScoreNetworkParameters(
+    prediction_head='non_conservative',
+    **common_mace_parameters)
 
 mlp_score_network_parameters = MLPScoreNetworkParameters(number_of_atoms=number_of_atoms,
                                                          n_hidden_dimensions=3,
                                                          hidden_dimensions_size=64)
 
-if model == 'mace':
+if model == 'diffusion_mace':
+    score_network_parameters = diffusion_mace_score_network_parameters
+elif model == 'mace_plus_prediction_head':
     score_network_parameters = mace_score_network_parameters
 elif model == 'mlp':
     score_network_parameters = mlp_score_network_parameters
@@ -76,14 +93,13 @@ acell = 5.5
 noise_parameters = NoiseParameters(total_time_steps=100, sigma_min=0.001, sigma_max=0.5)
 sampling_parameters = SamplingParameters(spatial_dimension=3,
                                          number_of_corrector_steps=1,
-                                         number_of_atoms=2,
+                                         number_of_atoms=number_of_atoms,
                                          number_of_samples=1000,
                                          sample_every_n_epochs=1,
                                          record_samples=True,
                                          cell_dimensions=[acell, acell, acell])
 
 
-# We will not optimize, so  this doesn't matter
 optimizer_parameters = OptimizerParameters(name=ValidOptimizerName.adamw, learning_rate=0.001, weight_decay=0.0)
 scheduler_parameters = ReduceLROnPlateauSchedulerParameters(name=ValidSchedulerName.reduce_lr_on_plateau,
                                                             factor=0.5,
@@ -106,11 +122,17 @@ tensorboard_logger = TensorBoardLogger(save_dir=output_directory,
 
 if __name__ == '__main__':
     torch.manual_seed(42)
+    # torch.set_default_dtype(torch.float64)
 
     box = acell * torch.ones(spatial_dimension)
 
-    equilibrium_relative_coordinates = torch.stack([0.25 * torch.ones(spatial_dimension),
-                                                    0.75 * torch.ones(spatial_dimension)])
+    if number_of_atoms == 1:
+        equilibrium_relative_coordinates = (0.5 * torch.ones(spatial_dimension)).unsqueeze(0)
+
+    elif number_of_atoms == 2:
+        equilibrium_relative_coordinates = torch.stack([0.25 * torch.ones(spatial_dimension),
+                                                        0.75 * torch.ones(spatial_dimension)])
+
     inverse_covariance = torch.zeros(number_of_atoms, spatial_dimension, number_of_atoms, spatial_dimension)
     for atom_i in range(number_of_atoms):
         for alpha in range(spatial_dimension):
@@ -155,6 +177,7 @@ if __name__ == '__main__':
                          max_epochs=max_epochs,
                          log_every_n_steps=1,
                          fast_dev_run=False,
+                         gradient_clip_val=gradient_clip_val,
                          logger=tensorboard_logger,
                          )
 
