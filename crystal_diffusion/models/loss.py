5735d7faa57fd312d0d678a1b4ab8b33fa92d4aa
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-06-17 14:36:11 -0400

    Weighted loss.

---
+++ crystal_diffusion/models/loss.py
@@ -0,0 +1,64 @@
+from dataclasses import dataclass
+
+import torch
+
+
+@dataclass(kw_only=True)
+class LossParameters:
+    """Specific Hyper-parameters for the loss function."""
+    algorithm: str = 'mse'
+    # The default values are chosen to lead to a flat loss curve vs. sigma, based on preliminary experiments.
+    sigma0: float = 0.2
+    exponent: float = 23.0259  # ~ 10 ln(10)
+
+
+class LossCalculator(torch.nn.Module):
+    """Class to calculate the loss."""
+    def __init__(self, hyperparams: LossParameters):
+        """Init method."""
+        super().__init__()
+
+        assert hyperparams.algorithm in {'mse', 'weighted_mse'}, f"Unknown loss algorithm '{hyperparams.algorithm}'"
+
+        self.hyperparams = hyperparams
+
+        if hyperparams.algorithm == 'mse':
+            self.mse_loss = torch.nn.MSELoss(reduction='mean')
+            self.compute_weights = False
+        else:  # weighted_mase
+            self.mse_loss = torch.nn.MSELoss(reduction='none')
+            self.compute_weights = True
+
+    @staticmethod
+    def _exponential_weights(sigmas, sigma0: float, exponent: float):
+        """Compute an exponential weight for the loss."""
+        weights = torch.exp(exponent * (sigmas - sigma0)) + 1.0
+        return weights
+
+    def calculate_loss(self, predicted_normalized_scores: torch.tensor,
+                       target_normalized_conditional_scores: torch.tensor,
+                       sigmas: torch.Tensor) -> torch.tensor:
+        """Calculate Loss.
+
+        All inputs are assumed to be tensors of dimension [batch_size, number_of_atoms, spatial_dimension]. In
+        particular, it is assumed that 'sigma' has been broadcast to the same shape as the scores.
+
+        Args:
+            predicted_normalized_scores : predicted scores
+            target_normalized_conditional_scores : the score targets
+            sigmas : the noise
+
+        Returns:
+            loss: a single number corresponding to the aggregated loss.
+        """
+        assert predicted_normalized_scores.shape == target_normalized_conditional_scores.shape == sigmas.shape, \
+            "Inconsistent shapes"
+
+        if self.compute_weights:
+            weights = self._exponential_weights(sigmas, self.hyperparams.sigma0, self.hyperparams.exponent)
+            unreduced_loss = self.mse_loss(predicted_normalized_scores, target_normalized_conditional_scores)
+            loss = torch.mean(unreduced_loss * weights)
+        else:
+            loss = self.mse_loss(predicted_normalized_scores, target_normalized_conditional_scores)
+
+        return loss
---
+++ tests/models/test_loss.py
@@ -0,0 +1,95 @@
+import pytest
+import torch
+
+from crystal_diffusion.models.loss import LossCalculator, LossParameters
+from crystal_diffusion.utils.tensor_utils import \
+    broadcast_batch_tensor_to_all_dimensions
+
+
+@pytest.fixture(scope="module", autouse=True)
+def set_random_seed():
+    torch.manual_seed(45233423)
+
+
+@pytest.fixture()
+def sigma0():
+    return 0.256
+
+
+@pytest.fixture()
+def exponent():
+    return 11.234
+
+
+@pytest.fixture()
+def batch_size():
+    return 12
+
+
+@pytest.fixture()
+def spatial_dimension():
+    return 3
+
+
+@pytest.fixture()
+def number_of_atoms():
+    return 8
+
+
+@pytest.fixture()
+def predicted_normalized_scores(batch_size, number_of_atoms, spatial_dimension):
+    return torch.rand(batch_size, number_of_atoms, spatial_dimension)
+
+
+@pytest.fixture()
+def target_normalized_conditional_scores(batch_size, number_of_atoms, spatial_dimension):
+    return torch.rand(batch_size, number_of_atoms, spatial_dimension)
+
+
+@pytest.fixture()
+def sigmas(batch_size, number_of_atoms, spatial_dimension):
+    batch_sigmas = torch.rand(batch_size)
+    shape = (batch_size, number_of_atoms, spatial_dimension)
+    sigmas = broadcast_batch_tensor_to_all_dimensions(batch_values=batch_sigmas, final_shape=shape)
+    return sigmas
+
+
+@pytest.fixture()
+def weights(sigmas, sigma0, exponent):
+    return 1.0 + torch.exp(exponent * (sigmas - sigma0))
+
+
+@pytest.fixture()
+def loss_parameters(algorithm, sigma0, exponent):
+    return LossParameters(algorithm=algorithm, sigma0=sigma0, exponent=exponent)
+
+
+@pytest.fixture()
+def loss_calculator(loss_parameters):
+    return LossCalculator(loss_parameters)
+
+
+@pytest.fixture()
+def computed_loss(loss_calculator, predicted_normalized_scores, target_normalized_conditional_scores, sigmas):
+    loss = loss_calculator.calculate_loss(predicted_normalized_scores,
+                                          target_normalized_conditional_scores,
+                                          sigmas)
+
+    return loss
+
+
+@pytest.fixture()
+def expected_loss(algorithm, weights, predicted_normalized_scores, target_normalized_conditional_scores, sigmas):
+    if algorithm == 'mse':
+        loss = torch.nn.functional.mse_loss(
+            predicted_normalized_scores, target_normalized_conditional_scores, reduction="mean"
+        )
+    else:
+        return torch.mean(weights * (predicted_normalized_scores - target_normalized_conditional_scores)**2)
+
+    return loss
+
+
+@pytest.mark.parametrize("algorithm", ['mse', 'weighted_mse'])
+def test_mse_loss(computed_loss, expected_loss):
+    torch.testing.assert_close(computed_loss, expected_loss)
