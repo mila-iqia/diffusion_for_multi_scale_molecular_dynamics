ded817601bcef7e15a0bf9a0825b7872abf96cb3
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-07-16 19:27:39 -0400

    Clean run script.

--- examples/local/diffusion/run_diffusion.sh
+++ examples/local/diffusion/run_diffusion.sh
@@ -3,11 +3,12 @@
 # This example assumes that the dataset 'si_diffusion_small' is present locally in the DATA folder.
 # It is also assumed that the user has a Comet account for logging experiments.
 
-CONFIG=../../config_files/diffusion/config_diffusion_mace.yaml
+CONFIG=../../config_files/diffusion/config_diffusion_egnn.yaml
 DATA_DIR=../../../data/si_diffusion_1x1x1
 PROCESSED_DATA=${DATA_DIR}/processed
-DATA_WORK_DIR=./tmp_work_dir/
-OUTPUT=output
+DATA_WORK_DIR=${DATA_DIR}/cache/
+
+OUTPUT=output/run1
 
 python ../../../crystal_diffusion/train_diffusion.py \
     --config $CONFIG \
