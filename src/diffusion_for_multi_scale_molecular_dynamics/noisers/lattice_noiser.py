ffec518a8ed8e5f5868065adf80a8d119b6f14b4
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-10-26 15:25:21 -0400

    Noisers

---
+++ src/diffusion_for_multi_scale_molecular_dynamics/noisers/lattice_noiser.py
@@ -0,0 +1,22 @@
+import torch
+
+
+class LatticeNoiser:
+    """Lattice noiser.
+
+    This class provides methods to generate noisy lattices.
+    TODO this is a placeholder
+    """
+    @staticmethod
+    def get_noisy_lattice_vectors(real_lattice_vectors: torch.Tensor) -> torch.Tensor:
+        """Get noisy lattice vectors.
+
+        TODO this is a placeholder
+
+        Args:
+            real_lattice_vectors: lattice vectors from the sampled data
+
+        Returns:
+            real_lattice_vectors: a sample of noised lattice vectors. Placeholder for now.
+        """
+        return real_lattice_vectors
