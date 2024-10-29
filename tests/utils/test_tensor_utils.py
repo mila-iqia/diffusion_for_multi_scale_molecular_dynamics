b1f0c6f1f94ec6f5b097ad2822f2a96a47b34bdc
Simon Blackburn <simon.blackburn@mila.quebec>
2024-10-29 13:43:54 -0400

    tensor utils unit tests

--- tests/utils/test_tensor_utils.py
+++ tests/utils/test_tensor_utils.py
@@ -1,8 +1,10 @@
 import pytest
 import torch
 
-from diffusion_for_multi_scale_molecular_dynamics.utils.tensor_utils import \
-    broadcast_batch_tensor_to_all_dimensions
+from diffusion_for_multi_scale_molecular_dynamics.utils.tensor_utils import (
+    broadcast_batch_matrix_tensor_to_all_dimensions,
+    broadcast_batch_tensor_to_all_dimensions,
+)
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -13,6 +15,11 @@ def set_random_seed():
 @pytest.fixture()
 def batch_values(batch_size):
     return torch.rand(batch_size)
+
+
+@pytest.fixture()
+def batch_matrix_values(batch_size, num_classes):
+    return torch.rand(batch_size, num_classes, num_classes)
 
 
 @pytest.fixture()
@@ -34,5 +41,22 @@ def test_broadcast_batch_tensor_to_all_dimensions(
     value_arrays = broadcast_values.reshape(batch_size, -1)
 
     for expected_value, computed_values in zip(batch_values, value_arrays):
+        expected_values = torch.ones_like(computed_values) * expected_value
+        torch.testing.assert_close(expected_values, computed_values)
+
+
+@pytest.mark.parametrize("batch_size", [4, 8])
+@pytest.mark.parametrize("number_of_dimensions", [1, 2, 3])
+@pytest.mark.parametrize("num_classes", [1, 2, 4])
+def test_broadcast_batch_matrix_tensor_to_all_dimensions(
+    batch_size, batch_matrix_values, final_shape, num_classes
+):
+    broadcast_values = broadcast_batch_matrix_tensor_to_all_dimensions(
+        batch_matrix_values, final_shape
+    )
+
+    value_arrays = broadcast_values.reshape(batch_size, -1, num_classes, num_classes)
+
+    for expected_value, computed_values in zip(batch_matrix_values, value_arrays):
         expected_values = torch.ones_like(computed_values) * expected_value
         torch.testing.assert_close(expected_values, computed_values)
