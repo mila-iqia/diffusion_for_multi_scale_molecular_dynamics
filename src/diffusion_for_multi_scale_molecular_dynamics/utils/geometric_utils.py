cb239c159c5d1e96a39f158e390ba1a94340dc88
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-11-05 14:27:37 -0500

    Moving useful method to a better place.

---
+++ src/diffusion_for_multi_scale_molecular_dynamics/utils/geometric_utils.py
@@ -0,0 +1,20 @@
+import itertools
+
+import torch
+
+
+def get_cubic_point_group_symmetries():
+    """Get cubic point group symmetries."""
+    permutations = [
+        torch.diag(torch.ones(3))[[idx]] for idx in itertools.permutations([0, 1, 2])
+    ]
+    sign_changes = [
+        torch.diag(torch.tensor(diag))
+        for diag in itertools.product([-1.0, 1.0], repeat=3)
+    ]
+    symmetries = []
+    for permutation in permutations:
+        for sign_change in sign_changes:
+            symmetries.append(permutation @ sign_change)
+
+    return symmetries
