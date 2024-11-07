00af358106ee2ce8ee9e11b81bbf0858086af332
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-11-07 15:42:45 -0500

    New test battery explicitly for Equivariance.

---
+++ tests/models/score_network/conftest.py
@@ -0,0 +1,47 @@
+import pytest
+import torch
+
+
+class BaseTestScore:
+    """Base class defining common fixtures for all tests."""
+    @pytest.fixture(scope="class", autouse=True)
+    def set_default_type_to_float64(self):
+        torch.set_default_dtype(torch.float64)
+        yield
+        # this returns the default type to float32 at the end of all tests in this class in order
+        # to not affect other tests.
+        torch.set_default_dtype(torch.float32)
+
+    @pytest.fixture(scope="class", autouse=True)
+    def set_seed(self):
+        """Set the random seed."""
+        torch.manual_seed(234233)
+
+    @pytest.fixture()
+    def score_network_parameters(self, *args):
+        raise NotImplementedError("This fixture must be implemented in the derived class.")
+
+    @pytest.fixture()
+    def score_network(self, *args):
+        raise NotImplementedError("This fixture must be implemented in the derived class.")
+
+    @pytest.fixture()
+    def batch_size(self, *args, **kwargs):
+        return 16
+
+    @pytest.fixture()
+    def number_of_atoms(self):
+        return 8
+
+    @pytest.fixture()
+    def spatial_dimension(self):
+        return 3
+
+    @pytest.fixture()
+    def num_atom_types(self):
+        return 5
+
+    @pytest.fixture()
+    def atom_types(self, batch_size, number_of_atoms, num_atom_types):
+        atom_types = torch.randint(0, num_atom_types + 1, (batch_size, number_of_atoms))
+        return atom_types
