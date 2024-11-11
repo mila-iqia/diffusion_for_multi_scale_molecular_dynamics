45ac9d5adcd0eae1479d01a5bb7cfa669cd961c2
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-11-11 12:05:40 -0500

    revamp the Si 1x1x1 data creation scripts

--- data/si_diffusion_1x1x1/create_data.sh
+++ data/si_diffusion_1x1x1/create_data.sh
@@ -8,6 +8,8 @@ CROP=10000
 NTRAIN_RUN=10
 NVALID_RUN=5
 
+SW_PATH="../../stillinger_weber_coefficients/Si.sw"
+
 NRUN=$(($NTRAIN_RUN + $NVALID_RUN))
 
 # Generate the data
@@ -20,17 +22,17 @@ for SEED in $(seq 1 $NRUN); do
   echo "Creating LAMMPS data for ${MODE}_run_${SEED}..."
   mkdir -p "${MODE}_run_${SEED}"
   cd "${MODE}_run_${SEED}"
-  lmp  -echo none -screen none < ../in.si.lammps -v STEP $(($STEP + $CROP)) -v T $TEMPERATURE -v S $BOX_SIZE -v SEED $SEED
+  lmp  -echo none -screen none < ../in.Si.lammps -v STEP $(($STEP + $CROP)) -v T $TEMPERATURE -v S $BOX_SIZE -v SEED $SEED -v SW_PATH $SW_PATH
 
   # extract the thermodynamic outputs in a yaml file
   egrep  '^(keywords:|data:$|---$|\.\.\.$|  - \[)' log.lammps > thermo_log.yaml
 
   mkdir -p "uncropped_outputs"
-  mv "dump.si-${TEMPERATURE}-${BOX_SIZE}.yaml" uncropped_outputs/
+  mv "dump.Si-${TEMPERATURE}-${BOX_SIZE}.yaml" uncropped_outputs/
   mv thermo_log.yaml uncropped_outputs/
 
   python ../../crop_lammps_outputs.py \
-      --lammps_yaml "uncropped_outputs/dump.si-${TEMPERATURE}-${BOX_SIZE}.yaml" \
+      --lammps_yaml "uncropped_outputs/dump.Si-${TEMPERATURE}-${BOX_SIZE}.yaml" \
       --lammps_thermo "uncropped_outputs/thermo_log.yaml" \
       --crop $CROP \
       --output_dir ./
