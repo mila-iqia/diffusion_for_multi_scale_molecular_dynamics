673127374ae46387c5e78e834197add2665a7147
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-11-11 12:37:00 -0500

    simpler script

---
+++ data/SiGe_diffusion_1x1x1/create_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+TEMPERATURE=300
+BOX_SIZE=1
+MAX_ATOM=8
+STEP=10000
+CROP=10000
+NTRAIN_RUN=10
+NVALID_RUN=5
+
+SW_PATH="../stillinger_weber_coefficients/SiGe.sw"
+IN_PATH="in.SiGe.lammps"
+
+create_data_function $TEMPERATURE $BOX_SIZE $MAX_ATOM $STEP $CROP $NTRAIN_RUN $NVALID_RUN $SW_PATH $IN_PATH
