f1400eba84c4ad7b64f77ca84f0ec4a6a8c8a1fc
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-11-11 12:43:10 -0500

    Revamped Si 2x2x2.

---
+++ data/Si_diffusion_2x2x2/create_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source ../data_generation_functions.sh
+
+TEMPERATURE=300
+BOX_SIZE=2
+MAX_ATOM=64
+STEP=10000
+CROP=10000
+NTRAIN_RUN=10
+NVALID_RUN=5
+
+SW_PATH="../stillinger_weber_coefficients/Si.sw"
+IN_PATH="in.Si.lammps"
+
+create_data_function $TEMPERATURE $BOX_SIZE $MAX_ATOM $STEP $CROP $NTRAIN_RUN $NVALID_RUN $SW_PATH $IN_PATH
