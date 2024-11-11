541c180c4498767c864c51b35e972190351febed
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-11-11 12:47:57 -0500

    Revamped Si 3x3x3.

---
+++ data/Si_diffusion_3x3x3/create_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source ../data_generation_functions.sh
+
+TEMPERATURE=300
+BOX_SIZE=3
+MAX_ATOM=216
+STEP=10000
+CROP=10000
+NTRAIN_RUN=10
+NVALID_RUN=5
+SW_PATH="../stillinger_weber_coefficients/Si.sw"
+IN_PATH="in.Si.lammps"
+
+create_data_function $TEMPERATURE $BOX_SIZE $MAX_ATOM $STEP $CROP $NTRAIN_RUN $NVALID_RUN $SW_PATH $IN_PATH
