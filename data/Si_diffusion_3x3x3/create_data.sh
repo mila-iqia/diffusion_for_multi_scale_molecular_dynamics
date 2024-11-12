0dd1a0885afcfa91a9e969b38ffe598e25f89613
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-11-12 08:55:55 -0500

    update the data creation scripts

--- data/Si_diffusion_3x3x3/create_data.sh
+++ data/Si_diffusion_3x3x3/create_data.sh
@@ -4,12 +4,13 @@ source ../data_generation_functions.sh
 
 TEMPERATURE=300
 BOX_SIZE=3
-MAX_ATOM=216
 STEP=10000
 CROP=10000
 NTRAIN_RUN=10
 NVALID_RUN=5
+
 SW_PATH="../stillinger_weber_coefficients/Si.sw"
 IN_PATH="in.Si.lammps"
+CONFIG_PATH="config.yaml"
 
-create_data_function $TEMPERATURE $BOX_SIZE $MAX_ATOM $STEP $CROP $NTRAIN_RUN $NVALID_RUN $SW_PATH $IN_PATH
+create_data_function $TEMPERATURE $BOX_SIZE $STEP $CROP $NTRAIN_RUN $NVALID_RUN $SW_PATH $IN_PATH $CONFIG_PATH
