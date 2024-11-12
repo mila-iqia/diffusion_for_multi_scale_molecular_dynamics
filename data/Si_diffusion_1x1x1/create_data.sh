8581feba95789b7b7f74a2c11f49461914eb43cf
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-11-12 08:39:45 -0500

    updated Si 1x1x1 data generation scripts

--- data/Si_diffusion_1x1x1/create_data.sh
+++ data/Si_diffusion_1x1x1/create_data.sh
@@ -4,7 +4,6 @@ source ../data_generation_functions.sh
 
 TEMPERATURE=300
 BOX_SIZE=1
-MAX_ATOM=8
 STEP=10000
 CROP=10000
 NTRAIN_RUN=10
@@ -12,5 +11,6 @@ NVALID_RUN=5
 
 SW_PATH="../stillinger_weber_coefficients/Si.sw"
 IN_PATH="in.Si.lammps"
+CONFIG_PATH="config.yaml"
 
-create_data_function $TEMPERATURE $BOX_SIZE $MAX_ATOM $STEP $CROP $NTRAIN_RUN $NVALID_RUN $SW_PATH $IN_PATH
+create_data_function $TEMPERATURE $BOX_SIZE $STEP $CROP $NTRAIN_RUN $NVALID_RUN $SW_PATH $IN_PATH $CONFIG_PATH
