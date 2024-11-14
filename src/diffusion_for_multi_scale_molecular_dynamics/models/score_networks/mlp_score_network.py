f3ac95296ed3a4f44ca84e2b520867c19b7deaf0
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-11-13 21:12:16 -0500

    Instantiate for device discoverability.

--- src/diffusion_for_multi_scale_molecular_dynamics/models/score_networks/mlp_score_network.py
+++ src/diffusion_for_multi_scale_molecular_dynamics/models/score_networks/mlp_score_network.py
@@ -87,15 +87,13 @@ class MLPScoreNetwork(ScoreNetwork):
             )
         self.non_linearity = nn.ReLU()
 
-        self.output_layers = AXL(
-            A=nn.Linear(
-                hyper_params.hidden_dimensions_size, atom_type_output_dimension
-            ),
-            X=nn.Linear(
-                hyper_params.hidden_dimensions_size, coordinate_output_dimension
-            ),
-            L=nn.Identity(),  # TODO placeholder
-        )
+        # Create nn object to be discoverable to be placed on the correct device
+        output_A_layer = nn.Linear(hyper_params.hidden_dimensions_size, atom_type_output_dimension)
+        output_X_layer = nn.Linear(hyper_params.hidden_dimensions_size, coordinate_output_dimension)
+        output_L_layer = nn.Identity()
+        self.output_layers = AXL(A=output_A_layer,
+                                 X=output_X_layer,
+                                 L=output_L_layer)  # TODO placeholder
 
     def _check_batch(self, batch: Dict[AnyStr, torch.Tensor]):
         super(MLPScoreNetwork, self)._check_batch(batch)
