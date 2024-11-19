02eaf7124a43301e0ca272f56cee267f3992b57b
Bruno Rousseau <bruno.rousseau@mila.quebec>
2024-11-19 12:26:04 -0500

    Create cif files with ovito.

--- src/diffusion_for_multi_scale_molecular_dynamics/utils/sample_trajectory.py
+++ src/diffusion_for_multi_scale_molecular_dynamics/utils/sample_trajectory.py
@@ -1,7 +1,12 @@
 from collections import defaultdict
+from pathlib import Path
 from typing import Any, Dict, NamedTuple, Union
 
+import einops
+import numpy as np
 import torch
+
+from diffusion_for_multi_scale_molecular_dynamics.namespace import AXL
 
 
 class SampleTrajectory:
@@ -37,3 +42,52 @@ class SampleTrajectory:
         """Write data to pickle file."""
         with open(path_to_pickle, "wb") as fd:
             torch.save(self._internal_data, fd)
+
+
+def get_predictor_trajectory(pickle_path: Path) -> AXL:
+    """Get predictor trajectory.
+
+    Args:
+        pickle_path: location of the output of a sample_trajectory object for a Langevin generator.
+
+    Returns:
+        trajectory_axl: trajectory composition object, where each field has dimension [nsamples, time, ...]
+    """
+    data = torch.load(pickle_path, map_location=torch.device("cpu"))
+
+    predictor_data = data["predictor_step"]
+
+    # The recording might have taken place over multiple batches. Combine corresponding compositions.
+    multiple_batch_compositions = defaultdict(list)
+    for entry in predictor_data:
+        time_index = entry["time_step_index"]
+        axl_composition = entry["composition_im1"]
+        multiple_batch_compositions[time_index].append(axl_composition)
+
+    list_time_indices = np.sort(np.array(list(multiple_batch_compositions.keys())))[
+        ::-1
+    ]
+
+    list_compositions = []
+    for time_index in list_time_indices:
+        batch_compositions = multiple_batch_compositions[time_index]
+        composition = AXL(
+            A=torch.vstack([c.A for c in batch_compositions]),
+            X=torch.vstack([c.X for c in batch_compositions]),
+            L=torch.vstack([c.L for c in batch_compositions]),
+        )
+        list_compositions.append(composition)
+
+    atoms_types = einops.rearrange(
+        [c.A for c in list_compositions], "time batch natoms -> batch time natoms"
+    )
+    relative_coordinates = einops.rearrange(
+        [c.X for c in list_compositions],
+        "time batch natoms space -> batch time natoms space",
+    )
+    lattice = einops.rearrange(
+        [c.L for c in list_compositions], "time batch d1 d2-> batch time d1 d2"
+    )
+    trajectory_axl = AXL(A=atoms_types, X=relative_coordinates, L=lattice)
+
+    return trajectory_axl
