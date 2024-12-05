b2b60f1c3fd7e26cd6afe1b947714e92132cdad7
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-12-05 09:42:56 -0500

    More sophisticated sampling constraint.

--- src/diffusion_for_multi_scale_molecular_dynamics/generators/sampling_constraint.py
+++ src/diffusion_for_multi_scale_molecular_dynamics/generators/sampling_constraint.py
@@ -2,64 +2,71 @@ import dataclasses
 from dataclasses import dataclass
 from pathlib import Path
 
+import einops
 import torch
 
 from diffusion_for_multi_scale_molecular_dynamics.namespace import AXL
+from diffusion_for_multi_scale_molecular_dynamics.noise_schedulers.noise_parameters import \
+    NoiseParameters
+from diffusion_for_multi_scale_molecular_dynamics.noise_schedulers.noise_scheduler import \
+    NoiseScheduler
+from diffusion_for_multi_scale_molecular_dynamics.noisers.atom_types_noiser import \
+    AtomTypesNoiser
+from diffusion_for_multi_scale_molecular_dynamics.noisers.relative_coordinates_noiser import \
+    RelativeCoordinatesNoiser
+from diffusion_for_multi_scale_molecular_dynamics.utils.d3pm_utils import \
+    class_index_to_onehot
 
 
 @dataclass
-class SamplingConstraint:
-    """Dataclass to hold the constraints for constrained sampling."""
-    total_time_steps: int  # The total number of time steps in denoising
+class SamplingConstraintParameters:
+    """Dataclass describing how constraints should be created."""
+
+    noise_parameters: NoiseParameters
+
+    # number of atom types excluding MASK
+    num_atom_types: int
 
     # the time step index at which free diffusion starts.
     # Should be equal or smaller to total_time_steps
     starting_free_diffusion_time_step: int
 
-    # There should be one AXL reference per time step.
-    # Tensors should have shape [time, natoms, ...]
-    reference_compositions: AXL
-
     # Indices of the atoms that should not diffuse freely.
     constrained_atom_indices: torch.Tensor
 
+    # reference composition at t=0
+    reference_composition: AXL
+
     def __post_init__(self):
         """Post init method, to validate input."""
-        assert self.total_time_steps > 0, "Time steps should be larger than 0."
         assert (
-            self.starting_free_diffusion_time_step > 0
-        ), "Time steps should be larger than 0."
-
-        assert (
-            self.starting_free_diffusion_time_step <= self.total_time_steps
+            self.starting_free_diffusion_time_step
+            <= self.noise_parameters.total_time_steps
         ), "Starting free diffusion time step should be smaller or equal to the total number of time steps."
 
         assert (
-            type(self.reference_compositions.X) is torch.Tensor
-        ), "the reference compositions should torch Tensors."
-        assert (
-            type(self.reference_compositions.A) is torch.Tensor
-        ), "the reference compositions should torch Tensors."
-        assert (
-            type(self.reference_compositions.L) is torch.Tensor
-        ), "the reference compositions should torch Tensors."
+            self.num_atom_types >= 0
+        ), "The number of atom types should be non-negative."
 
-        assert len(self.reference_compositions.X.shape) == 3, "X has the wrong shape."
-        assert len(self.reference_compositions.A.shape) == 2, "A has the wrong shape."
-
-        number_of_time_steps_, number_of_atoms = self.reference_compositions.A.shape
         assert (
-            number_of_time_steps_ == self.total_time_steps
-        ), "The number of time steps in the reference compositions should be total_number_of_time_steps."
-
-        number_of_time_steps_, number_of_atoms_, _ = self.reference_compositions.X.shape
+            type(self.reference_composition.X) is torch.Tensor
+        ), "the reference composition should be made of torch Tensors."
         assert (
-            number_of_time_steps_ == self.total_time_steps
-        ), "The number of time steps in the reference compositions should be total_number_of_time_steps."
+            type(self.reference_composition.A) is torch.Tensor
+        ), "the reference composition should be made of torch Tensors."
+        assert (
+            type(self.reference_composition.L) is torch.Tensor
+        ), "the reference composition should be made of torch Tensors."
+
+        assert len(self.reference_composition.X.shape) == 2, "X has the wrong shape."
+        assert len(self.reference_composition.A.shape) == 1, "A has the wrong shape."
+
+        (number_of_atoms,) = self.reference_composition.A.shape
+        number_of_atoms_, _ = self.reference_composition.X.shape
 
         assert (
             number_of_atoms_ == number_of_atoms
-        ), "The number of atoms in the reference compositions should be consistent between X and A."
+        ), "The number of atoms in the reference composition should be consistent between X and A."
 
         indices_are_good = torch.logical_and(
             self.constrained_atom_indices >= 0,
@@ -68,11 +75,121 @@ class SamplingConstraint:
 
         assert (
             indices_are_good
-        ), "the constrained_atom_indices are inconsistent with the reference compositions."
+        ), "the constrained_atom_indices are inconsistent with the reference composition."
+
+
+@dataclass
+class SamplingConstraint:
+    """Dataclass to hold the constraints for constrained sampling."""
+
+    sampling_constraint_parameters: SamplingConstraintParameters
+
+    # There should be one AXL reference per time step.
+    # Tensors should have shape [time, natoms, ...]
+    constraint_compositions: AXL
+
+    def __post_init__(self):
+        """Post init method, to validate input."""
+        assert (
+            type(self.constraint_compositions.X) is torch.Tensor
+        ), "the reference compositions should torch Tensors."
+        assert (
+            type(self.constraint_compositions.A) is torch.Tensor
+        ), "the reference compositions should torch Tensors."
+        assert (
+            type(self.constraint_compositions.L) is torch.Tensor
+        ), "the reference compositions should torch Tensors."
+
+        assert len(self.constraint_compositions.X.shape) == 3, "X has the wrong shape."
+        assert len(self.constraint_compositions.A.shape) == 2, "A has the wrong shape."
+
+        number_of_time_steps_, number_of_atoms = self.constraint_compositions.A.shape
+        assert (
+            number_of_time_steps_
+            == self.sampling_constraint_parameters.noise_parameters.total_time_steps
+        ), "The number of time steps in the constraint compositions should be total_number_of_time_steps."
+
+        number_of_time_steps_, number_of_atoms_, _ = (
+            self.constraint_compositions.X.shape
+        )
+        assert (
+            number_of_time_steps_
+            == self.sampling_constraint_parameters.noise_parameters.total_time_steps
+        ), "The number of time steps in the constraint compositions should be total_number_of_time_steps."
+
+        assert (
+            number_of_atoms_ == number_of_atoms
+        ), "The number of atoms in the constraint compositions should be consistent between X and A."
+
+
+def create_sampling_constraint(
+    sampling_constraint_parameters: SamplingConstraintParameters,
+) -> SamplingConstraint:
+    """Create sampling constraint."""
+    total_time_steps = sampling_constraint_parameters.noise_parameters.total_time_steps
+    # Account for the MASK class
+    num_classes = sampling_constraint_parameters.num_atom_types + 1
+
+    sampler = NoiseScheduler(
+        sampling_constraint_parameters.noise_parameters, num_classes=num_classes
+    )
+    noise, _ = sampler.get_all_sampling_parameters()
+
+    relative_coordinates_noiser = RelativeCoordinatesNoiser()
+    atom_type_noiser = AtomTypesNoiser()
+
+    a_0 = sampling_constraint_parameters.reference_composition.A
+    x_0 = sampling_constraint_parameters.reference_composition.X
+    l_0 = sampling_constraint_parameters.reference_composition.L
+
+    one_hot_a_0 = class_index_to_onehot(a_0, num_classes=num_classes)
+    number_of_atoms = x_0.shape[0]
+
+    coordinates_broadcasting = torch.ones_like(x_0)
+
+    # Use the same Z-scores for all noising. This insures that all noised structures are "close" to each other.
+    z_scores = RelativeCoordinatesNoiser._get_gaussian_noise(x_0.shape)
+
+    list_x = [x_0]
+    list_a = [a_0]
+    list_l = [l_0]
+
+    for time_idx in torch.arange(1, total_time_steps):
+        sigmas_i = noise.sigma[time_idx] * coordinates_broadcasting
+        q_bar_matrices_i = einops.repeat(
+            noise.q_bar_matrix[time_idx],
+            "n1 n2 -> natoms n1 n2",
+            natoms=number_of_atoms,
+        )
+
+        x_i = relative_coordinates_noiser.get_noisy_relative_coordinates_sample_given_z_scores(
+            x_0, sigmas_i, z_scores
+        )
+
+        a_i = atom_type_noiser.get_noisy_atom_types_sample(
+            one_hot_a_0, q_bar_matrices_i
+        )
+
+        list_x.append(x_i)
+        list_a.append(a_i)
+        list_l.append(l_0)  # TODO : take care of L correctly.
+
+    constraint_compositions = AXL(
+        A=torch.stack(list_a, dim=0),
+        X=torch.stack(list_x, dim=0),
+        L=torch.stack(list_l, dim=0),
+    )
+
+    sampling_constraint = SamplingConstraint(
+        sampling_constraint_parameters=sampling_constraint_parameters,
+        constraint_compositions=constraint_compositions,
+    )
+
+    return sampling_constraint
 
 
 def write_sampling_constraint(
-    sampling_constraints: SamplingConstraint, output_path: Path
+    sampling_constraint: SamplingConstraint, output_path: Path
 ):
     """Write sampling constraint.
 
@@ -84,7 +201,7 @@ def write_sampling_constraint(
         None.
     """
     # Write to disk as a dictionary to avoid conflicts if code changes.
-    torch.save(dataclasses.asdict(sampling_constraints), output_path)
+    torch.save(dataclasses.asdict(sampling_constraint), output_path)
 
 
 def read_sampling_constraint(output_path: Path) -> SamplingConstraint:
@@ -97,4 +214,20 @@ def read_sampling_constraint(output_path: Path) -> SamplingConstraint:
         sampling_constraint: object read from file.
     """
     sampling_constraint_dict = torch.load(output_path)
-    return SamplingConstraint(**sampling_constraint_dict)
+
+    sampling_constraint_parameters_dict = sampling_constraint_dict[
+        "sampling_constraint_parameters"
+    ]
+    noise_parameters_dict = sampling_constraint_parameters_dict.pop("noise_parameters")
+    noise_parameters = NoiseParameters(**noise_parameters_dict)
+
+    sampling_constraint_parameters = SamplingConstraintParameters(
+        noise_parameters=noise_parameters, **sampling_constraint_parameters_dict
+    )
+
+    constraint_compositions = sampling_constraint_dict["constraint_compositions"]
+
+    return SamplingConstraint(
+        sampling_constraint_parameters=sampling_constraint_parameters,
+        constraint_compositions=constraint_compositions,
+    )
--- tests/generators/test_sampling_constraint.py
+++ tests/generators/test_sampling_constraint.py
@@ -1,11 +1,15 @@
 from pathlib import Path
 
+import einops
 import pytest
 import torch
 
 from diffusion_for_multi_scale_molecular_dynamics.generators.sampling_constraint import (
-    SamplingConstraint, read_sampling_constraint, write_sampling_constraint)
+    SamplingConstraintParameters, create_sampling_constraint,
+    read_sampling_constraint, write_sampling_constraint)
 from diffusion_for_multi_scale_molecular_dynamics.namespace import AXL
+from diffusion_for_multi_scale_molecular_dynamics.noise_schedulers.noise_parameters import \
+    NoiseParameters
 
 
 @pytest.fixture()
@@ -19,8 +23,13 @@ def starting_free_diffusion_time_step():
 
 
 @pytest.fixture()
-def number_of_atom_classes():
+def number_of_atom_types():
     return 4
+
+
+@pytest.fixture()
+def num_classes(number_of_atom_types):
+    return number_of_atom_types + 1
 
 
 @pytest.fixture()
@@ -34,29 +43,32 @@ def spatial_dimensions():
 
 
 @pytest.fixture()
+def noise_parameters(total_time_steps):
+    return NoiseParameters(total_time_steps=total_time_steps)
+
+
+@pytest.fixture()
 def constrained_atom_indices(number_of_atoms):
     return torch.randperm(number_of_atoms)[: number_of_atoms // 2].to(torch.long)
 
 
 @pytest.fixture()
-def reference_relative_coordinates(
-    total_time_steps, number_of_atoms, spatial_dimensions
-):
-    return torch.rand(total_time_steps, number_of_atoms, spatial_dimensions)
+def reference_relative_coordinates(number_of_atoms, spatial_dimensions):
+    return torch.rand(number_of_atoms, spatial_dimensions)
 
 
 @pytest.fixture()
-def reference_atom_types(total_time_steps, number_of_atoms, number_of_atom_classes):
-    return torch.randint(0, number_of_atom_classes, (total_time_steps, number_of_atoms))
+def reference_atom_types(number_of_atoms, number_of_atom_types):
+    return torch.randint(0, number_of_atom_types, (number_of_atoms, ))
 
 
 @pytest.fixture()
-def reference_lattice(total_time_steps, spatial_dimensions):
-    return torch.rand(total_time_steps, spatial_dimensions, spatial_dimensions)
+def reference_lattice(spatial_dimensions):
+    return torch.rand(spatial_dimensions, spatial_dimensions)
 
 
 @pytest.fixture()
-def reference_compositions(
+def reference_composition(
     reference_relative_coordinates, reference_atom_types, reference_lattice
 ):
     return AXL(
@@ -65,41 +77,61 @@ def reference_compositions(
 
 
 @pytest.fixture()
-def sampling_constraint(
-    total_time_steps,
-    starting_free_diffusion_time_step,
-    reference_compositions,
-    constrained_atom_indices,
-):
+def sampling_constraint_parameters(noise_parameters, number_of_atom_types, starting_free_diffusion_time_step,
+                                   constrained_atom_indices, reference_composition):
 
-    sampling_constraints = SamplingConstraint(
-        total_time_steps=total_time_steps,
-        starting_free_diffusion_time_step=starting_free_diffusion_time_step,
-        reference_compositions=reference_compositions,
-        constrained_atom_indices=constrained_atom_indices,
-    )
-    return sampling_constraints
+    return SamplingConstraintParameters(noise_parameters=noise_parameters,
+                                        num_atom_types=number_of_atom_types,
+                                        starting_free_diffusion_time_step=starting_free_diffusion_time_step,
+                                        constrained_atom_indices=constrained_atom_indices,
+                                        reference_composition=reference_composition)
 
 
-def test_write_sampling_constraint(tmp_path, sampling_constraint):
+def test_sampling_constraint_parameters(sampling_constraint_parameters, reference_composition):
+    torch.testing.assert_close(sampling_constraint_parameters.reference_composition, reference_composition)
+
+
+def test_create_sampling_constraint(sampling_constraint_parameters, reference_composition, num_classes):
+    sampling_constraint = create_sampling_constraint(sampling_constraint_parameters)
+
+    # Check that the t=0 composition is consistent with the reference composition.
+    torch.testing.assert_close(sampling_constraint.constraint_compositions.X[0], reference_composition.X)
+    torch.testing.assert_close(sampling_constraint.constraint_compositions.A[0], reference_composition.A)
+    torch.testing.assert_close(sampling_constraint.constraint_compositions.L[0], reference_composition.L)
+
+    # Check that the noised atom types are either the reference or MASK
+    reference_a = einops.repeat(reference_composition.A, "natoms -> ntimes natoms",
+                                ntimes=sampling_constraint_parameters.noise_parameters.total_time_steps)
+    noised_a = sampling_constraint.constraint_compositions.A
+
+    masked_class_index = num_classes - 1
+    same_class = noised_a == reference_a
+    mask_class = noised_a == masked_class_index
+
+    assert torch.logical_or(same_class, mask_class).all()
+
+
+def test_write_sampling_constraint(tmp_path, sampling_constraint_parameters):
+
+    sampling_constraint = create_sampling_constraint(sampling_constraint_parameters)
+
     output_path = Path(tmp_path) / "test_sampling_constraint.pkl"
     write_sampling_constraint(sampling_constraint, output_path)
     new_sampling_constraint = read_sampling_constraint(output_path)
 
-    assert (
-        new_sampling_constraint.total_time_steps == sampling_constraint.total_time_steps
-    )
-    assert (
-        new_sampling_constraint.starting_free_diffusion_time_step
-        == sampling_constraint.starting_free_diffusion_time_step
+    new_sampling_constraint_parameters = new_sampling_constraint.sampling_constraint_parameters
+
+    assert new_sampling_constraint_parameters.num_atom_types == sampling_constraint_parameters.num_atom_types
+
+    assert (new_sampling_constraint_parameters.starting_free_diffusion_time_step
+            == sampling_constraint_parameters.starting_free_diffusion_time_step)
+
+    torch.testing.assert_close(
+        sampling_constraint.sampling_constraint_parameters.constrained_atom_indices,
+        new_sampling_constraint.sampling_constraint_parameters.constrained_atom_indices,
     )
 
     torch.testing.assert_close(
-        sampling_constraint.constrained_atom_indices,
-        new_sampling_constraint.constrained_atom_indices,
-    )
-
-    torch.testing.assert_close(
-        sampling_constraint.reference_compositions,
-        new_sampling_constraint.reference_compositions,
+        sampling_constraint.constraint_compositions,
+        new_sampling_constraint.constraint_compositions,
     )
