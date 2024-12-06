c52d0691baa3d82fc32ee4c85c5ce71ee7eb6ef1
Simon Blackburn <simonblackburn@AP-T-042.local>
2024-12-06 12:24:31 -0500

    forget to fix a typo

--- src/diffusion_for_multi_scale_molecular_dynamics/utils/noise_utils.py
+++ src/diffusion_for_multi_scale_molecular_dynamics/utils/noise_utils.py
@@ -5,5 +5,25 @@ def scale_sigma_by_number_of_atoms(
     sigma: torch.Tensor,
     number_of_atoms: torch.Tensor,
     spatial_dimension: int
-):
-    return sigma * torch.pow(number_of_atoms, 1 / spatial_dimension)
+) -> torch.Tensor:
+    r"""Scale noise factor by the number of atoms.
+
+    The variance of the noise distribution for cartesian coordinates depends on the size of the unit cell. If we assume
+    the volume of a unit cell is proportional to the number of atoms, we can mitigate this variance by rescaling the
+    factor :math:`\sigma` in the relative coordinates space by the number of atoms.
+
+    .. math::
+
+        \sigma(n) = \frac{\sigma}{\sqrt[d]n}
+
+    with :math:`d`  the number of spatial dimensions.
+
+    Args:
+        sigma: unscaled noise factor :math:`\sigma` as a [batch_size, ...] tensor
+        number_of_atoms: number of atoms in the unit cell as a [batch_size, ...] tensor
+        spatial_dimension: number of spatial dimensions
+
+    Returns:
+        sigma_n : scaled sigma
+    """
+    return sigma / torch.pow(number_of_atoms, 1 / spatial_dimension)
