d24b1faaf53ab4740e668a03723d7187e5af3475
Simon Blackburn <simonblackburn@AP-T-042.local>
2024-12-06 08:08:10 -0500

    score tests

--- src/diffusion_for_multi_scale_molecular_dynamics/score/gaussian_score.py
+++ src/diffusion_for_multi_scale_molecular_dynamics/score/gaussian_score.py
@@ -9,11 +9,8 @@ The Gaussian takes the form
 
 The score is defined as S = nabla_l ln K(l, l0).
 """
-import numpy as np
-import torch
 
-from diffusion_for_multi_scale_molecular_dynamics.score.wrapped_gaussian_score import \
-    get_coordinates_sigma_normalized_score
+import torch
 
 
 def get_lattice_sigma_normalized_score(
@@ -21,9 +18,8 @@ def get_lattice_sigma_normalized_score(
 ) -> torch.Tensor:
     r"""Get the sigma normalized score for lattice parameters.
 
-    We compute this from a normal (non-wrapped) gaussian kernel, which we can get by calling a wrapped gaussian kernel
-    with k_max = 0 (which effectively removes the sum). Because we are using a variance-preserving approach for the
-    lattice parameters, the width of the gaussian kernel is rescaled by the noise parameter :math:`alpha_bar`.
+    We compute this from a normal (non-wrapped) gaussian kernel.  Because we are using a variance-preserving approach
+    for the lattice parameters, the width of the gaussian kernel is rescaled by the noise parameter :math:`alpha_bar`.
 
     .. math::
 
@@ -37,15 +33,15 @@ def get_lattice_sigma_normalized_score(
         alpha_bar : the cumulative variance in the definition of the variance-preserving diffusion.
 
     Returns:
-        sigma_normalized_score : the value of the sigma normalized score.
+        sigma_score : the value of the sigma normalized score.
     """
     # rescale sigma by 1 - alpha_bar
-    sigma_effective = np.sqrt(1 - alpha_bar) * sigma_n
-    # we can get the sigma normalized score using the wrapped gaussian method with k_max = 0 i.e. no sum
-    # and using the effective sigma computed
-    sigma_normalized_score = get_coordinates_sigma_normalized_score(
-        delta_l,
-        sigma_effective,
-        kmax=0
-    )
-    return sigma_normalized_score
+    sigma_effective = torch.sqrt(1 - alpha_bar) * sigma_n
+
+    # we can get the sigma normalized score using a method similar to the brute force calculation in the wrapped
+    # gaussian kernel implementation. We do not need a sum over k as the kernel is not periodic. This simplifies to the
+    # -x / sigma
+
+    sigma_score = -delta_l / sigma_effective
+
+    return sigma_score
--- tests/score/test_wrapped_gaussian_score.py
+++ tests/score/test_wrapped_gaussian_score.py
@@ -7,7 +7,8 @@ from diffusion_for_multi_scale_molecular_dynamics.score.wrapped_gaussian_score i
     _get_s1b_exponential, _get_sigma_normalized_s2,
     _get_sigma_square_times_score_1_from_exponential,
     _get_small_sigma_large_u_mask, _get_small_sigma_small_u_mask,
-    get_coordinates_sigma_normalized_score, get_sigma_normalized_score_brute_force)
+    get_coordinates_sigma_normalized_score,
+    get_sigma_normalized_score_brute_force)
 
 
 @pytest.fixture(scope="module", autouse=True)
