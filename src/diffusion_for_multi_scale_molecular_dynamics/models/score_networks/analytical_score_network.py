f183212b030ee04170cf04d85fddea78fefa5d8d
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-12-22 15:03:40 -0500

    Corrected a minor import issue.

--- src/diffusion_for_multi_scale_molecular_dynamics/models/score_networks/analytical_score_network.py
+++ src/diffusion_for_multi_scale_molecular_dynamics/models/score_networks/analytical_score_network.py
@@ -22,7 +22,7 @@ from diffusion_for_multi_scale_molecular_dynamics.models.score_networks.score_ne
 from diffusion_for_multi_scale_molecular_dynamics.namespace import (
     AXL, NOISE, NOISY_AXL_COMPOSITION)
 from diffusion_for_multi_scale_molecular_dynamics.score.wrapped_gaussian_score import (
-    get_log_wrapped_gaussians, get_sigma_normalized_score)
+    get_log_wrapped_gaussians, get_coordinates_sigma_normalized_score)
 from diffusion_for_multi_scale_molecular_dynamics.utils.basis_transformations import \
     map_relative_coordinates_to_unit_cell
 from diffusion_for_multi_scale_molecular_dynamics.utils.symmetry_utils import \
@@ -191,7 +191,7 @@ class AnalyticalScoreNetwork(ScoreNetwork):
         # score. However, the normalized score thus obtained is improperly normalized.
         # the empirical scores are normalized with the time-dependent sigmas; the "data" sigma
         # are unknown (or even ill-defined) in general!
-        list_effective_sigma_normalized_scores = get_sigma_normalized_score(
+        list_effective_sigma_normalized_scores = get_coordinates_sigma_normalized_score(
             u, repeated_effective_sigmas, self.kmax
         )
         list_scores = list_effective_sigma_normalized_scores / repeated_effective_sigmas
@@ -283,9 +283,128 @@ class AnalyticalScoreNetwork(ScoreNetwork):
         )
 
         # Mimic perfect predictions of single possible atomic type.
+<<<<<<< HEAD
         atomic_logits = torch.zeros(
             batch_size, self.natoms, self.number_of_atomic_classes
         )
+=======
+        atomic_logits = torch.zeros(batch_size, self.natoms, self.number_of_atomic_classes)
+        atomic_logits[..., -1] = -torch.inf
+
+        axl_scores = AXL(
+            A=atomic_logits,
+            X=sigma_normalized_scores,
+            L=torch.zeros_like(sigma_normalized_scores),
+        )
+
+        return axl_scores
+
+    def _compute_unnormalized_log_probability(
+        self, sigmas: torch.Tensor, xt: torch.Tensor, x_eq: torch.Tensor
+    ) -> torch.Tensor:
+
+        batch_size = sigmas.shape[0]
+
+        # Recast various spatial arrays to the correct dimensions to combine them,
+        # in dimensions [batch, nd, number_of_translations]
+        effective_variance = einops.repeat(
+            sigmas**2 + self.sigma_d_square,
+            "batch 1 -> batch nd t",
+            t=self.number_of_translations,
+            nd=self.nd,
+        )
+
+        sampling_coordinates = einops.repeat(
+            xt,
+            "batch natom d -> batch (natom d) t",
+            batch=batch_size,
+            t=self.number_of_translations,
+        )
+
+        equilibrium_coordinates = einops.repeat(
+            x_eq,
+            "natom d -> batch (natom d) t",
+            batch=batch_size,
+            t=self.number_of_translations,
+        )
+
+        translations = einops.repeat(
+            self.translations_k, "t -> batch nd t", batch=batch_size, nd=self.nd
+        )
+
+        exponent = (
+            -0.5
+            * (sampling_coordinates - equilibrium_coordinates - translations) ** 2
+            / effective_variance
+        )
+        # logsumexp on lattice translation vectors, then sum on spatial indices
+        unnormalized_log_prob = torch.logsumexp(exponent, dim=2, keepdim=False).sum(
+            dim=1
+        )
+
+        return unnormalized_log_prob
+
+
+class TargetScoreBasedAnalyticalScoreNetwork(AnalyticalScoreNetwork):
+    """Target Score-Based Analytical Score Network.
+
+    An analytical score network that leverages the computation of the target for the score network.
+    This can only work if the permutation equivariance is turned off. This should produce exactly the same results
+    as the AnalyticalScoreNetwork, but does not require gradient calculation.
+    """
+
+    def __init__(self, hyper_params: AnalyticalScoreNetworkParameters):
+        """__init__.
+
+        Args:
+            hyper_params : hyper parameters from the config file.
+        """
+        super(TargetScoreBasedAnalyticalScoreNetwork, self).__init__(hyper_params)
+        assert (
+            not hyper_params.use_permutation_invariance
+        ), "This implementation is only valid in the absence of permutation equivariance."
+        self.x0 = self.all_x0[0]
+
+    def _forward_unchecked(
+        self, batch: Dict[AnyStr, Any], conditional: bool = False
+    ) -> torch.Tensor:
+        """Forward unchecked.
+
+        This method assumes that the input data has already been checked with respect to expectations
+        and computes the scores assuming that the data is in the correct format.
+
+        Args:
+            batch : dictionary containing the data to be processed by the model.
+            conditional (optional): CURRENTLY DOES NOTHING.
+
+        Returns:
+            output : the scores computed by the model as a [batch_size, n_atom, spatial_dimension] tensor.
+        """
+        sigmas = batch[NOISE]  # dimension: [batch_size, 1]
+        xt = batch[NOISY_AXL_COMPOSITION].X
+        batch_size = xt.shape[0]
+
+        broadcast_sigmas = einops.repeat(
+            sigmas,
+            "batch 1 -> batch natoms spatial_dimension",
+            natoms=self.natoms,
+            spatial_dimension=self.spatial_dimension,
+        )
+
+        broadcast_effective_sigmas = (broadcast_sigmas**2 + self.sigma_d_square).sqrt()
+
+        delta_relative_coordinates = map_relative_coordinates_to_unit_cell(xt - self.x0)
+        misnormalized_scores = get_coordinates_sigma_normalized_score(
+            delta_relative_coordinates, broadcast_effective_sigmas, kmax=self.kmax
+        )
+
+        sigma_normalized_scores = (
+            broadcast_sigmas / broadcast_effective_sigmas * misnormalized_scores
+        )
+
+        # Mimic perfect predictions of single possible atomic type.
+        atomic_logits = torch.zeros(batch_size, self.natoms, self.number_of_atomic_classes)
+>>>>>>> 9e3e741 (Corrected a minor import issue.)
         atomic_logits[..., -1] = -torch.inf
 
         axl_scores = AXL(
