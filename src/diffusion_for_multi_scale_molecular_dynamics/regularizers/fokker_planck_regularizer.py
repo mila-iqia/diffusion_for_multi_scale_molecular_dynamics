674d4a67390b54a13989340eb74a6e092e341ef5
Bruno Rousseau <rousseau.bruno@gmail.com>
2024-12-28 15:59:17 -0500

    A score fokker planck regularizer.

---
+++ src/diffusion_for_multi_scale_molecular_dynamics/regularizers/fokker_planck_regularizer.py
@@ -0,0 +1,245 @@
+from typing import Callable, Dict
+
+import einops
+import torch
+
+from diffusion_for_multi_scale_molecular_dynamics.models.score_networks import \
+    ScoreNetwork
+from diffusion_for_multi_scale_molecular_dynamics.namespace import (
+    AXL, CARTESIAN_FORCES, NOISE, NOISY_AXL_COMPOSITION, TIME, UNIT_CELL)
+from diffusion_for_multi_scale_molecular_dynamics.noise_schedulers.exploding_variance import \
+    SigmaCalculator
+
+
+class FokkerPlanckRegularizer:
+    """Fokker-Planck Regularizer.
+
+    This class implements a modified version of the 'score Fokker-Planck' residual for the purpose
+    of regularizing a score network.
+
+    Autodiff is used for the time derivative and the first order space derivative. The
+    Hutchinson trace estimator is used for the second order space derivative.
+    """
+
+    def __init__(self, sigma_min: float, sigma_max: float, number_of_hte_terms: int):
+        """Init method."""
+        self.sigma_calculator = SigmaCalculator(
+            sigma_min=sigma_min, sigma_max=sigma_max
+        )
+        self.number_of_hte_terms = number_of_hte_terms
+
+    def _create_batch(
+        self,
+        relative_coordinates: torch.Tensor,
+        times: torch.Tensor,
+        atom_types: torch.Tensor,
+        unit_cells: torch.Tensor,
+    ) -> Dict[str, torch.Tensor]:
+        sigmas_t = self.sigma_calculator(times)
+
+        forces = torch.zeros_like(relative_coordinates)
+
+        composition = AXL(A=atom_types, X=relative_coordinates, L=unit_cells)
+
+        batch = {
+            NOISY_AXL_COMPOSITION: composition,
+            NOISE: sigmas_t,
+            TIME: times,
+            UNIT_CELL: unit_cells,
+            CARTESIAN_FORCES: forces,
+        }
+        return batch
+
+    def _create_score_function(
+        self,
+        score_network: ScoreNetwork,
+        atom_types: torch.Tensor,
+        unit_cells: torch.Tensor,
+    ):
+        """Create score function.
+
+        Args:
+            score_network: a Score Network for which we seek the Fokker-Planck residual.
+            atom_types: atom types, held fixed.
+            unit_cells: unit cells, held fixed.
+
+        Returns:
+            score_function: a callable with input (relative_coordinates, time) which computes the scores, for
+                atom_types and unit_cells held fixed.
+        """
+        def score_function(relative_coordinates, times):
+            batch_size, natoms, spatial_dimension = relative_coordinates.shape
+            batch = self._create_batch(
+                relative_coordinates, times, atom_types, unit_cells
+            )
+
+            sigmas_t = einops.repeat(
+                self.sigma_calculator(times),
+                "batch 1 -> batch natoms d",
+                natoms=natoms,
+                d=spatial_dimension,
+            )
+
+            sigma_normalized_scores = score_network(batch, conditional=False).X
+            scores = sigma_normalized_scores / sigmas_t
+            return scores
+
+        return score_function
+
+    def _create_rademacher_random_variables(
+        self, batch_size, num_atoms, spatial_dimension
+    ):
+        # A rademacher random variable can only take values in {-1, +1}.
+        rademacher = (
+            2
+            * torch.randint(
+                2, (self.number_of_hte_terms, batch_size, num_atoms, spatial_dimension)
+            )
+            - 1.0
+        )
+        return rademacher
+
+    @staticmethod
+    def get_hte_laplacian_term(
+        score_function_x: Callable,
+        relative_coordinates: torch.Tensor,
+        rademacher_z: torch.Tensor,
+    ):
+        """Get HTE Laplacian term.
+
+        Compute a term in the Hutchinson trace estimator (HTE) of the laplacian.
+
+        Args:
+            score_function_x: the score function, restricted to a single input, the relative_coordinates.
+            relative_coordinates: spatial input for the score function at which we seek the Laplacian.
+            rademacher_z: a rademacher random variable, assumed to have a shape compatible with `relative_coordinates`.
+
+        Returns:
+            laplacian_term: one term in the HTE estimate of the Laplacian
+        """
+
+        def jvp_with_z_func(x):
+            return torch.func.jvp(score_function_x, (x,), (rademacher_z,))[1]
+
+        approximate_score_laplacian = torch.func.jvp(
+            jvp_with_z_func, (relative_coordinates,), (rademacher_z,)
+        )[1]
+        return approximate_score_laplacian
+
+    def compute_residual_components(
+        self,
+        score_network: ScoreNetwork,
+        batch: Dict[str, torch.Tensor],
+        rademacher_z: torch.Tensor,
+    ):
+        """Compute residual components.
+
+        This method computes all the terms that are needed to compute the Fokker-Planck residual.
+
+        Args:
+            score_network: the score network.
+            batch: the batch for which we compute the residual components.
+            rademacher_z: rademacher random variables, of
+                dimension [number_of_hte_terms, (relative_coordinates_dimension)].
+
+        Returns:
+            scores: the scores.
+            scores_time_derivative: the scores time derivative.
+            scores_divergence_scores: the term score cdot nabla score.
+            approximate_scores_laplacian: an HTE appraximation to nabla^2 score.
+        """
+        relative_coordinates = batch[NOISY_AXL_COMPOSITION].X
+        times = batch[TIME]
+        atom_types = batch[NOISY_AXL_COMPOSITION].A
+        unit_cells = batch[UNIT_CELL]
+
+        score_function = self._create_score_function(
+            score_network=score_network, atom_types=atom_types, unit_cells=unit_cells
+        )
+        scores = score_function(relative_coordinates, times)
+
+        def _batched_summed_score_function(relative_coordinates, times):
+            # A convenience function that sums over the batch dimension. Since all the
+            # batch elements are independent, differentiating with respect to this function is
+            # equivalent to differentiating the original score_function, but minus the extra batch dimensions
+            # in the Jacobian tensors.
+            return score_function(relative_coordinates, times).sum(dim=0)
+
+        # The _batch_summed_score_function has
+        #   - the time input has dimension [batch_size, 1]
+        #   - the output has dimension [number_of_atoms, spatial_dimension] : the batch is summed over!
+        # The Jacobian tensor below will thus have dimensions [number_of_atoms, spatial_dimension, batch_size, 1]
+        time_jacobian = torch.func.jacrev(_batched_summed_score_function, argnums=1)(
+            relative_coordinates, times
+        )
+
+        # Rearrange the dimensions so that we output a tensor of
+        #   dimension [batch_size, number_of_atims, spatial_dimension]
+        scores_time_derivative = time_jacobian.squeeze(-1).permute(2, 0, 1)
+
+        def score_function_x(relative_coordinates):
+            # A convenience function that only explicitly depends on the relative coordinates
+            return score_function(relative_coordinates, times)
+
+        # This is the (S \dot nabla) S term
+        scores_divergence_scores = torch.func.jvp(
+            score_function_x, (relative_coordinates,), (scores,)
+        )[1]
+
+        list_z = rademacher_z.to(relative_coordinates.device)
+
+        list_laplacian_terms = [
+            self.get_hte_laplacian_term(score_function_x, relative_coordinates, z)
+            for z in list_z
+        ]
+
+        approximate_scores_laplacian = torch.stack(list_laplacian_terms).mean(dim=0)
+
+        return (
+            scores,
+            scores_time_derivative,
+            scores_divergence_scores,
+            approximate_scores_laplacian,
+        )
+
+    def compute_score_fokker_planck_residuals(
+        self, score_network: ScoreNetwork, batch: Dict[str, torch.Tensor]
+    ):
+        """Compute score Fokker-Planck residual.
+
+        Args:
+            score_network: the score network.
+            batch: a batch containing all that is required to call the score network.
+
+        Returns:
+            residuals: the score Fokker-Planck residual.
+        """
+        batch_size, natoms, spatial_dimension = batch[NOISY_AXL_COMPOSITION].X.shape
+
+        times = batch[TIME]
+
+        sigma = self.sigma_calculator.get_sigma(times)
+        sigma_dot = self.sigma_calculator.get_sigma_time_derivative(times)
+
+        sigma_term = einops.repeat(
+            sigma * sigma_dot,
+            "batch 1 -> batch natoms space",
+            natoms=natoms,
+            space=spatial_dimension,
+        )
+
+        rademacher_z = self._create_rademacher_random_variables(
+            batch_size, natoms, spatial_dimension
+        )
+        (
+            scores,
+            scores_time_derivative,
+            scores_divergence_scores,
+            approximate_scores_laplacian,
+        ) = self.compute_residual_components(score_network, batch, rademacher_z)
+
+        residuals = scores_time_derivative - sigma_term * (
+            2.0 * scores_divergence_scores + approximate_scores_laplacian
+        )
+
+        return residuals
---
+++ tests/regularizers/test_fokker_planck_regularizer.py
@@ -0,0 +1,244 @@
+import einops
+import pytest
+import torch
+
+from diffusion_for_multi_scale_molecular_dynamics.regularizers.fokker_planck_regularizer import \
+    FokkerPlanckRegularizer
+from tests.regularizers.differentiable_score_network import (
+    DifferentiableScoreNetwork, DifferentiableScoreNetworkParameters)
+
+
+class TestFokkerPlanckRegularizer:
+
+    @pytest.fixture(scope="class", autouse=True)
+    def set_default_type_to_float64(self):
+        torch.set_default_dtype(torch.float64)
+        yield
+        # this returns the default type to float32 at the end of all tests in this class in order
+        # to not affect other tests.
+        torch.set_default_dtype(torch.float32)
+
+    @pytest.fixture(scope="class", autouse=True)
+    def set_seed(self):
+        """Set the random seed."""
+        torch.manual_seed(34534234)
+
+    @pytest.fixture()
+    def sigma_min(self):
+        return 0.001
+
+    @pytest.fixture()
+    def sigma_max(self):
+        return 0.2
+
+    @pytest.fixture()
+    def number_of_hte_terms(self):
+        return 4
+
+    @pytest.fixture()
+    def number_of_atoms(self):
+        return 4
+
+    @pytest.fixture()
+    def num_atom_types(self):
+        return 1
+
+    @pytest.fixture()
+    def spatial_dimension(self):
+        return 3
+
+    @pytest.fixture()
+    def batch_size(self):
+        return 16
+
+    @pytest.fixture()
+    def relative_coordinates(self, batch_size, number_of_atoms, spatial_dimension):
+        return torch.rand(batch_size, number_of_atoms, spatial_dimension)
+
+    @pytest.fixture()
+    def times(self, batch_size):
+        return torch.rand(batch_size, 1)
+
+    @pytest.fixture()
+    def atom_types(self, batch_size, number_of_atoms):
+        return torch.zeros(batch_size, number_of_atoms, dtype=torch.int64)
+
+    @pytest.fixture()
+    def unit_cells(self, batch_size, spatial_dimension):
+        acell = 5.0
+        unit_cells = torch.diag(acell * torch.ones(spatial_dimension)).repeat(
+            batch_size, 1, 1
+        )
+        return unit_cells
+
+    @pytest.fixture()
+    def score_parameters(
+        self, number_of_atoms, num_atom_types, spatial_dimension, sigma_min, sigma_max
+    ):
+        score_parameters = DifferentiableScoreNetworkParameters(
+            number_of_atoms=number_of_atoms,
+            spatial_dimension=spatial_dimension,
+            num_atom_types=num_atom_types,
+            sigma_min=sigma_min,
+            sigma_max=sigma_max,
+        )
+        return score_parameters
+
+    @pytest.fixture()
+    def score_network(self, score_parameters):
+        return DifferentiableScoreNetwork(score_parameters)
+
+    @pytest.fixture()
+    def regularizer(self, sigma_min, sigma_max, number_of_hte_terms):
+        return FokkerPlanckRegularizer(
+            sigma_min=sigma_min,
+            sigma_max=sigma_max,
+            number_of_hte_terms=number_of_hte_terms,
+        )
+
+    @pytest.fixture()
+    def score_function(self, regularizer, score_network, atom_types, unit_cells):
+        score_function = regularizer._create_score_function(
+            score_network, atom_types, unit_cells
+        )
+        return score_function
+
+    def test_score_function(
+        self, score_network, score_function, relative_coordinates, times
+    ):
+        computed_scores = score_function(relative_coordinates, times)
+        expected_scores = score_network._score_function(relative_coordinates, times)
+        torch.testing.assert_allclose(computed_scores, expected_scores)
+
+    def test_create_rademacher_random_variables(
+        self,
+        regularizer,
+        number_of_hte_terms,
+        batch_size,
+        number_of_atoms,
+        spatial_dimension,
+    ):
+        z = regularizer._create_rademacher_random_variables(
+            batch_size, number_of_atoms, spatial_dimension
+        )
+        assert z.shape == (
+            number_of_hte_terms,
+            batch_size,
+            number_of_atoms,
+            spatial_dimension,
+        )
+        torch.testing.assert_close(z.unique(), torch.tensor([-1.0, 1.0]))
+
+    def test_get_hte_laplacian_term(
+        self, score_network, regularizer, score_function, relative_coordinates, times
+    ):
+        batch_size, num_atoms, spatial_dimension = relative_coordinates.shape
+
+        def score_function_x(relative_coordinates):
+            return score_function(relative_coordinates, times)
+
+        rademacher_z = regularizer._create_rademacher_random_variables(
+            batch_size=batch_size,
+            num_atoms=num_atoms,
+            spatial_dimension=spatial_dimension,
+        )
+
+        exact_hessian = score_network._space_hessian_function(
+            relative_coordinates, times
+        )
+
+        for z in rademacher_z:
+            computed_laplacian = regularizer.get_hte_laplacian_term(
+                score_function_x, relative_coordinates, z
+            )
+            hz = einops.einsum(
+                exact_hessian,
+                z,
+                "batch ni si nj sj nk sk, batch nk sk -> batch ni si nj sj",
+            )
+            expected_laplacian = einops.einsum(
+                z, hz, "batch nj sj, batch ni si nj sj -> batch ni si"
+            )
+            torch.testing.assert_allclose(computed_laplacian, expected_laplacian)
+
+    def test_compute_residual_components(
+        self,
+        regularizer,
+        score_network,
+        relative_coordinates,
+        times,
+        atom_types,
+        unit_cells,
+    ):
+        batch = regularizer._create_batch(
+            relative_coordinates, times, atom_types, unit_cells
+        )
+        rademacher_z = regularizer._create_rademacher_random_variables(
+            *relative_coordinates.shape
+        )
+
+        (
+            scores,
+            scores_time_derivative,
+            scores_divergence_scores,
+            approximate_scores_laplacian,
+        ) = regularizer.compute_residual_components(score_network, batch, rademacher_z)
+
+        expected_scores = score_network._score_function(relative_coordinates, times)
+        expected_scores_time_derivatives = score_network._time_derivative_function(
+            relative_coordinates, times
+        )
+
+        space_jacobian = score_network._space_jacobian_function(
+            relative_coordinates, times
+        )
+        expected_scores_divergence_scores = einops.einsum(
+            space_jacobian,
+            expected_scores,
+            "batch ni si nj sj, batch nj sj -> batch ni si",
+        )
+
+        expected_approximate_scores_laplacian = torch.zeros_like(expected_scores)
+
+        exact_hessian = score_network._space_hessian_function(
+            relative_coordinates, times
+        )
+
+        n_hte = len(rademacher_z)
+        for z in rademacher_z:
+            hz = einops.einsum(
+                exact_hessian,
+                z,
+                "batch ni si nj sj nk sk, batch nk sk -> batch ni si nj sj",
+            )
+            zhz = einops.einsum(z, hz, "batch nj sj, batch ni si nj sj -> batch ni si")
+            expected_approximate_scores_laplacian += zhz / n_hte
+
+        torch.testing.assert_allclose(scores, expected_scores)
+        torch.testing.assert_allclose(
+            scores_time_derivative, expected_scores_time_derivatives
+        )
+        torch.testing.assert_allclose(
+            scores_divergence_scores, expected_scores_divergence_scores
+        )
+        torch.testing.assert_allclose(
+            approximate_scores_laplacian, expected_approximate_scores_laplacian
+        )
+
+    def test_compute_score_fokker_planck_residuals(
+        self,
+        regularizer,
+        score_network,
+        relative_coordinates,
+        times,
+        atom_types,
+        unit_cells,
+    ):
+        batch = regularizer._create_batch(
+            relative_coordinates, times, atom_types, unit_cells
+        )
+        residuals = regularizer.compute_score_fokker_planck_residuals(
+            score_network, batch
+        )
+
+        assert residuals.shape == relative_coordinates.shape
