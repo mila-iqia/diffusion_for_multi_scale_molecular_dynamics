f1efc539f765d6eec549a849fa529fe93ca9f807
Bruno Rousseau <rousseau.bruno@gmail.com>
2025-01-31 16:19:34 -0500

    Lattice methods to create shells of bloch waves.

--- src/diffusion_for_multi_scale_molecular_dynamics/utils/lattice_utils.py
+++ src/diffusion_for_multi_scale_molecular_dynamics/utils/lattice_utils.py
@@ -1,6 +1,10 @@
 import itertools
+from typing import List
 
 import torch
+
+from diffusion_for_multi_scale_molecular_dynamics.utils.geometric_utils import \
+    get_cubic_point_group_symmetries
 
 
 def get_relative_coordinates_lattice_vectors(
@@ -23,3 +27,151 @@ def get_relative_coordinates_lattice_vectors(
     )
 
     return list_relative_lattice_vectors
+
+
+def _sort_complete_shell(complete_shell: torch.Tensor) -> torch.Tensor:
+    """Sort complete shell.
+
+    Sort the lattice vectors so that the most positive elements appear first.
+
+    Args:
+        complete_shell: a tensor of dimension [number of lattice vectors, spatial dimension].
+
+    Returns:
+        sorted shell: the complete shell, sorted.
+    """
+    number_of_lattice_vectors, spatial_dimension = complete_shell.shape
+
+    # Build a scalar quantity that will sort following the desired rules
+    ordering_scalar = torch.zeros(number_of_lattice_vectors)
+
+    for d in range(spatial_dimension):
+        column = complete_shell[:, d]
+
+        power = spatial_dimension - d - 1
+        factor = number_of_lattice_vectors**power
+
+        sorted_unique_values = column.unique().sort().values
+
+        for rank, unique_value in enumerate(sorted_unique_values):
+            idx = column == unique_value
+            ordering_scalar[idx] += rank * factor
+
+    order = torch.flip(ordering_scalar.argsort(), dims=(0,))
+
+    ordered_shell = complete_shell[order]
+    return ordered_shell
+
+
+def get_cubic_point_group_complete_lattice_shells(
+    number_of_complete_shells: int, spatial_dimension: int = 3
+) -> List[torch.Tensor]:
+    """Get cubic point group complete lattice shells.
+
+    This method creates lattice vectors organized in fully symmetric shells, sorted according to the
+    length of a shell member, excluding L=0. If there are length degeneracies, all degenerate shells are included.
+
+    The cubic point group is ASSUMED. This should be modified to deal with other point groups.
+
+    Args:
+        number_of_complete_shells: number of complete shells of lattice vectors to create.
+        spatial_dimension: the dimension of space.
+
+    Returns:
+        list_complete_shells: a list of complete, sorted lattice vector shells.
+    """
+    number_of_trial_shells = 2 * number_of_complete_shells
+    lattice_vectors = get_relative_coordinates_lattice_vectors(
+        number_of_trial_shells, spatial_dimension
+    )
+    # sort by their norm
+    squared_norms = (lattice_vectors**2).sum(-1)
+
+    # The vectors should be composed of integers
+    sorted_lattice_vectors = lattice_vectors[squared_norms.argsort()].int()
+
+    symmetries = get_cubic_point_group_symmetries(spatial_dimension).int()
+
+    number_of_created_shells = 0
+    known_set = set()
+
+    list_complete_shells = []
+
+    previous_shell_squared_norm = 0
+
+    # Exclude zero.
+    for lattice_vector in sorted_lattice_vectors[1:]:
+        if tuple(lattice_vector.numpy()) in known_set:
+            continue
+
+        new_shell_set = set(
+            tuple(ell) for ell in torch.matmul(symmetries, lattice_vector).numpy()
+        )
+        known_set.update(new_shell_set)
+        number_of_created_shells += 1
+
+        complete_shell = _sort_complete_shell(torch.tensor(list(new_shell_set)))
+
+        list_complete_shells.append(complete_shell)
+
+        shell_squared_norm = (lattice_vector**2).sum()
+
+        if (
+            len(list_complete_shells) >= number_of_complete_shells
+            and shell_squared_norm > previous_shell_squared_norm
+        ):
+            break
+        previous_shell_squared_norm = shell_squared_norm
+
+    return list_complete_shells
+
+
+def get_cubic_point_group_positive_normalized_bloch_wave_vectors(
+    number_of_complete_shells: int, spatial_dimension: int = 3
+) -> torch.Tensor:
+    """Get cubic point group positive normalized bloch wave vectors.
+
+    This method generates reciprocal lattice vectors for the cubic lattice. The vectors are "normalized" in
+    the sense that they are made up of integers, such that, for spatial_dimension = 3,
+
+        K[m] = m_1 b_1 + m_2 b_2 + m_3 b_3,  b_i = 2pi/a e_i
+
+    The "normalized" vectors are of the form [m_1, m_2,  m_3].
+
+    We assume that inversion is part of the point group. Thus, we can transform
+
+        {e^[i K r], e^[-i K r] } ->  {cos(K r), sin(K r) }
+
+    and so there is no need to manipulate complex numbers. Thus we wil only keep half of lattice vectors,
+    removing the half that is the image under inversion.
+
+    Args:
+        number_of_complete_shells: number of complete shells to consider
+        spatial_dimension: dimension of space
+
+    Returns:
+        positive_bloch_wave_vectors: half of complete shell normalized reciprocal lattice vectors.
+    """
+    list_complete_shells = get_cubic_point_group_complete_lattice_shells(
+        number_of_complete_shells, spatial_dimension
+    )
+    list_half_shells = []
+
+    inversion = -torch.eye(spatial_dimension).int()
+
+    for shell in list_complete_shells:
+        known_set = set()
+        half_shell = []
+        for lattice_vector in shell:
+            if tuple(lattice_vector.numpy()) in known_set:
+                continue
+
+            half_shell.append(lattice_vector)
+
+            inverse_lattice_vector = torch.matmul(inversion, lattice_vector)
+            s = {tuple(lattice_vector.numpy()), tuple(inverse_lattice_vector.numpy())}
+            known_set.update(s)
+        list_half_shells.append(torch.stack(half_shell))
+
+    positive_bloch_wave_vectors = torch.vstack(list_half_shells)
+    return positive_bloch_wave_vectors
--- tests/utils/test_lattice_utils.py
+++ tests/utils/test_lattice_utils.py
@@ -1,8 +1,17 @@
 import pytest
 import torch
 
-from diffusion_for_multi_scale_molecular_dynamics.utils.lattice_utils import \
-    get_relative_coordinates_lattice_vectors
+from diffusion_for_multi_scale_molecular_dynamics.utils.geometric_utils import \
+    get_cubic_point_group_symmetries
+from diffusion_for_multi_scale_molecular_dynamics.utils.lattice_utils import (
+    _sort_complete_shell, get_cubic_point_group_complete_lattice_shells,
+    get_cubic_point_group_positive_normalized_bloch_wave_vectors,
+    get_relative_coordinates_lattice_vectors)
+
+
+@pytest.fixture()
+def spatial_dimension():
+    return 3
 
 
 @pytest.mark.parametrize("number_of_shells", [1, 2, 3])
@@ -63,3 +72,66 @@ def test_get_relative_coordinates_lattice_vectors_3d(number_of_shells):
     )
 
     torch.testing.assert_close(expected_lattice_vectors, computed_lattice_vectors)
+
+
+def test_sort_complete_shell():
+    shell = torch.tensor(
+        [[1, 0, 0], [0, 0, 1], [0, 0, -1], [0, -1, 0], [0, 1, 0], [-1, 0, 0]],
+        dtype=torch.int32,
+    )
+
+    expected_sorted_shell = torch.tensor(
+        [[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, -1], [0, -1, 0], [-1, 0, 0]],
+        dtype=torch.int32,
+    )
+
+    sorted_shell = _sort_complete_shell(shell)
+
+    torch.testing.assert_close(expected_sorted_shell, sorted_shell)
+
+
+def test_get_cubic_point_group_complete_lattice_shells(spatial_dimension):
+    number_of_complete_shells = 3
+    list_shells = get_cubic_point_group_complete_lattice_shells(
+        number_of_complete_shells=number_of_complete_shells,
+        spatial_dimension=spatial_dimension,
+    )
+
+    symmetries = get_cubic_point_group_symmetries(spatial_dimension).int()
+
+    assert len(list_shells) >= number_of_complete_shells
+
+    for shell in list_shells:
+        computed_set = set(tuple(ell) for ell in shell.numpy())
+        expected_set = set(tuple(ell) for ell in torch.matmul(symmetries, shell[0]).numpy())
+        assert computed_set == expected_set
+
+
+def test_get_cubic_point_group_positive_normalized_bloch_wave_vectors(
+    spatial_dimension,
+):
+
+    number_of_complete_shells = 3
+    list_shells = get_cubic_point_group_complete_lattice_shells(
+        number_of_complete_shells, spatial_dimension
+    )
+
+    all_lattice_vectors = torch.vstack(list_shells)
+
+    bloch_wave_vectors = get_cubic_point_group_positive_normalized_bloch_wave_vectors(
+        number_of_complete_shells, spatial_dimension
+    )
+
+    assert len(bloch_wave_vectors) == len(all_lattice_vectors) / 2
+
+    expected_set = set(tuple(ell.numpy()) for ell in all_lattice_vectors)
+    computed_set = set(tuple(ell.numpy()) for ell in bloch_wave_vectors)
+    assert computed_set.issubset(expected_set)
+
+    inversion = -torch.eye(spatial_dimension).int()
+
+    inversion_bloch_wave_vectors = torch.matmul(bloch_wave_vectors, inversion)
+    inversion_set = set(tuple(ell.numpy()) for ell in inversion_bloch_wave_vectors)
+    computed_set.update(inversion_set)
+
+    assert computed_set == expected_set
