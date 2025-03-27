1707b37bfc8020669e198506a02320d71290220f
Bruno Rousseau <rousseau.bruno@gmail.com>
2025-03-27 14:52:47 -0400

    Renaming things

--- experiments/atom_types_only_experiments/patches/equilibrium_structure.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-from diffusion_for_multi_scale_molecular_dynamics.utils.reference_configurations import \
-    create_equilibrium_sige_structure
-
-if __name__ == "__main__":
-    output_file_path = Path(__file__).parent / "equilibrium_sige.cif"
-    structure = create_equilibrium_sige_structure()
-    structure.to(output_file_path)
