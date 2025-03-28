efb935afa1f4a550b56b61b6c4db8c076bcb4232
Bruno Rousseau <rousseau.bruno@gmail.com>
2025-03-28 14:16:16 -0400

    Update the run scripts.

--- experiments/toy_problems/experiments/consistency_with_analytical_guide_regularizer/run.sh
+++ experiments/toy_problems/experiments/consistency_with_analytical_guide_regularizer/run.sh
@@ -3,7 +3,14 @@
 export OMP_PATH="/opt/homebrew/opt/libomp/include/"
 export PYTORCH_ENABLE_MPS_FALLBACK=1
 
-CONFIG=config.yaml
 OUTPUT=./output/run1
+
+BASE_CONFIG=../base_config.yaml
+SPECIFIC_CONFIG=specific_config.yaml
+CONFIG=config.yaml
+
+# Build the config
+cp $BASE_CONFIG $CONFIG
+cat $SPECIFIC_CONFIG >> $CONFIG
 
 train_diffusion --accelerator "cpu" --config $CONFIG --output $OUTPUT 
