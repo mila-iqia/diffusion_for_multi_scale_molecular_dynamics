2aef0e8cf6b58c22be5188b8c7125f88b6515237
Bruno Rousseau <rousseau.bruno@gmail.com>
2025-06-10 10:57:45 -0400

    More sophisticated training approach.

--- src/diffusion_for_multi_scale_molecular_dynamics/active_learning_loop/trainer/flare_hyperparameter_optimizer.py
+++ src/diffusion_for_multi_scale_molecular_dynamics/active_learning_loop/trainer/flare_hyperparameter_optimizer.py
@@ -1,11 +1,135 @@
-from typing import Dict, Tuple
+from dataclasses import dataclass
+from typing import Tuple
 
+import numpy as np
 import pandas as pd
 from flare.bffs.sgp import SGP_Wrapper
-from flare.bffs.sgp.sparse_gp import (compute_negative_likelihood,
-                                      compute_negative_likelihood_grad_stable)
-from flare_pp import SparseGP
+from flare.bffs.sgp.sparse_gp import compute_negative_likelihood_grad_stable
 from scipy.optimize import OptimizeResult, minimize
+
+
+@dataclass(kw_only=True)
+class FlareOptimizerConfiguration:
+    """Flare Optimizer Configuration.
+
+    Various parameters defining how the hyperparameters of the FLARE sparce Gaussian process
+    should be optimized.
+    """
+    optimization_method: str = "BFGS"
+    max_optimization_iterations: int = 100
+
+    optimize_sigma: bool = True
+    optimize_sigma_e: bool = True
+    optimize_sigma_f: bool = True
+    optimize_sigma_s: bool = True
+
+    print: bool = False  # Should the scipy algorithm print progress to screen?
+    ftol: float = 1e-3
+    gtol: float = 1e-3
+
+    def __post_init__(self):
+        """Post init."""
+        assert self.optimization_method in ["BFGS", "L-BFGS-B", "nelder-mead"], \
+            f"Unknown optimization method {self.optimization_method}. Review input."
+
+        assert self.max_optimization_iterations >= 0, "The number of iterations should be non-negative."
+
+        assert self.optimize_sigma + self.optimize_sigma_e + self.optimize_sigma_f + self.optimize_sigma_s != 0, \
+            "All trainable parameters are set to FALSE. No training can take place! Review input."
+
+
+class FlareHyperparametersOptimizer:
+    """Class to drive the training of a FLARE sparse GP."""
+
+    def __init__(self, flare_optimizer_configuration: FlareOptimizerConfiguration):
+        """Init method."""
+        self.flare_optimizer_configuration = flare_optimizer_configuration
+
+        self._ordered_training_flags = [self.flare_optimizer_configuration.optimize_sigma,
+                                        self.flare_optimizer_configuration.optimize_sigma_e,
+                                        self.flare_optimizer_configuration.optimize_sigma_f,
+                                        self.flare_optimizer_configuration.optimize_sigma_s]
+
+        self._translator = HyperparameterTranslator(*self._ordered_training_flags)
+        self._optimization_method = self.flare_optimizer_configuration.optimization_method
+
+        self._requires_gradient = True
+        if self._optimization_method == "nelder-mead":
+            self._requires_gradient = False
+
+    def _create_function_to_minimize(self, sparse_gp):
+        sparse_gp.precompute_KnK()
+
+        def function_to_minimize(minimization_input: np.array):
+            starting_hyperparameters = 1.0 * sparse_gp.hyperparameters
+            hyperparameters = (
+                self._translator.generate_sgp_hyperparameters_from_minimization_inputs(starting_hyperparameters,
+                                                                                       minimization_input))
+            nll, grads = compute_negative_likelihood_grad_stable(hyperparameters, sparse_gp, precomputed=True)
+
+            if self._requires_gradient:
+                return nll, grads[self._ordered_training_flags]
+            else:
+                return nll
+
+        return function_to_minimize
+
+    def train(self, sgp_model: SGP_Wrapper) -> Tuple[OptimizeResult, pd.DataFrame]:
+        """Train.
+
+        This method trains the spare GP. Note that it has the SIDE EFFECT of
+        modifying the hyperparameters of the sparse GP in the sgp_model object.
+
+        Args:
+            sgp_model : a FLARE spare GP wrapper that contains a spare GP to be trained.
+
+        Returns:
+            optimization_results: the optimization result output of scipy.minimize.
+            history_df: a dataframe with the minimizing iterations.
+        """
+        function_to_minimize = self._create_function_to_minimize(sgp_model.sparse_gp)
+        initial_hyperparameters = sgp_model.sparse_gp.hyperparameters
+
+        optimization_tracker = OptimizationTracker(initial_hyperparameters=initial_hyperparameters,
+                                                   translator=self._translator)
+
+        initial_guess = initial_hyperparameters[self._ordered_training_flags]
+
+        options_dict = dict(maxiter=self.flare_optimizer_configuration.max_optimization_iterations,
+                            disp=self.flare_optimizer_configuration.print,
+                            ftol=self.flare_optimizer_configuration.ftol,
+                            gtol=self.flare_optimizer_configuration.gtol)
+
+        optimization_result = minimize(function_to_minimize,
+                                       initial_guess,
+                                       method=self._optimization_method,
+                                       jac=self._requires_gradient,
+                                       callback=optimization_tracker.callback,
+                                       options=options_dict)
+
+        history_df = optimization_tracker.get_optimization_history()
+
+        return optimization_result, history_df
+
+
+class HyperparameterTranslator:
+    """Hyperparameter translator.
+
+    This class is responsible for mapping the sgp hyperparameters to the inputs of the function to
+    be minimized to obtain the negative log likelihood.
+    """
+    def __init__(self, optimize_sigma: bool, optimize_sigma_e: bool, optimize_sigma_f: bool, optimize_sigma_s: bool):
+        """Init method."""
+        self._ordered_training_flags = [optimize_sigma, optimize_sigma_e, optimize_sigma_f, optimize_sigma_s]
+
+    def generate_sgp_hyperparameters_from_minimization_inputs(self, starting_hyperparameters: np.array,
+                                                              minimization_input: np.array) -> np.array:
+        """Generate SGP hyperparameters from minimization inputs."""
+        # The minimization input may have a different dimension as the hyperparameters.
+        new_inputs = np.zeros(len(starting_hyperparameters))
+        new_inputs[self._ordered_training_flags] = minimization_input
+        hyperparameters = np.where(self._ordered_training_flags, new_inputs, starting_hyperparameters)
+        return hyperparameters
 
 
 class OptimizationTracker:
@@ -14,8 +138,11 @@ class OptimizationTracker:
     This class will keep track of intermediate values os a scipy optimization
     algorithm through a callback method.
     """
-    def __init__(self):
+    def __init__(self, initial_hyperparameters: np.array, translator: HyperparameterTranslator):
         """Init method."""
+        self._initial_hyperparameters = initial_hyperparameters
+        self._translator = translator
+
         self.rows = []
 
     def get_optimization_history(self) -> pd.DataFrame:
@@ -32,74 +159,15 @@ class OptimizationTracker:
         #   - sigma_energy : the energy noise term
         #   - sigma_force : the force noise term
         #   - sigma_stress : the stress noise term
-        # where "noise" means that the GP predicts f, and the real lalbel is y ~ f + z * noise.
-        assert len(intermediate_result.x) == 4, "unexpected number of hyperparmeters."
-        row = dict(sigma=intermediate_result.x[0],
-                   sigma_energy=intermediate_result.x[1],
-                   sigma_forces=intermediate_result.x[2],
-                   sigma_stress=intermediate_result.x[3],
+        # where "noise" means that the GP predicts f, and the real label is y ~ f + z * noise.
+        minimization_input = intermediate_result.x
+        hyperparameters = (
+            self._translator.generate_sgp_hyperparameters_from_minimization_inputs(self._initial_hyperparameters,
+                                                                                   minimization_input))
+        row = dict(sigma=hyperparameters[0],
+                   sigma_energy=hyperparameters[1],
+                   sigma_forces=hyperparameters[2],
+                   sigma_stress=hyperparameters[3],
                    negative_log_likelihood=intermediate_result.fun)
+
         self.rows.append(row)
-
-
-class FlareHyperparametersOptimizer:
-    """Class to drive the training of a FLARE sparse GP."""
-
-    def __init__(self, method: str, minimize_options: Dict):
-        """Init method."""
-        assert method in ["BFGS", "L-BFGS-B", "nelder-mead"], f"Unknown method {method}"
-
-        self._minimize_options = minimize_options
-        self._method = method
-
-        self._requires_gradient = True
-        if self._method == "nelder-mead":
-            self._requires_gradient = False
-
-    def _create_function_to_minimize_no_gradient(self, sparse_gp: SparseGP):
-        def function_to_minimize(hyperparameters):
-            return compute_negative_likelihood(hyperparameters, sparse_gp, print_vals=True)
-        return function_to_minimize
-
-    def _create_function_to_minimize_with_gradient(self, sparse_gp):
-        sparse_gp.precompute_KnK()
-
-        def function_to_minimize(hyperparameters):
-            return compute_negative_likelihood_grad_stable(hyperparameters, sparse_gp, precomputed=True)
-        return function_to_minimize
-
-    def _create_function_to_minimize(self, sparse_gp):
-        if self._requires_gradient:
-            return self._create_function_to_minimize_with_gradient(sparse_gp)
-        else:
-            return self._create_function_to_minimize_no_gradient(sparse_gp)
-
-    def train(self, sgp_model: SGP_Wrapper) -> Tuple[OptimizeResult, pd.DataFrame]:
-        """Train.
-
-        This method trains the spare GP. Note that it has the SIDE EFFECT of
-        modifying the hyperparameters of the sparse GP in the sgp_model object.
-
-        Args:
-            sgp_model : a FLARE spare GP wrapper that contains a spare GP to be trained.
-
-        Returns:
-            optimization_results: the optimization result output of scipy.minimize.
-            history_df: a dataframe with the minimizing iterations.
-        """
-        function_to_minimize = self._create_function_to_minimize(sgp_model.sparse_gp)
-
-        optimization_tracker = OptimizationTracker()
-
-        initial_guess = sgp_model.sparse_gp.hyperparameters
-
-        optimization_result = minimize(function_to_minimize,
-                                       initial_guess,
-                                       method=self._method,
-                                       jac=self._requires_gradient,
-                                       callback=optimization_tracker.callback,
-                                       options=self._minimize_options)
-
-        history_df = optimization_tracker.get_optimization_history()
-
-        return optimization_result, history_df
--- tests/active_learning_loop/trainer/test_flare_hyperparameter_optimizer.py
+++ tests/active_learning_loop/trainer/test_flare_hyperparameter_optimizer.py
@@ -1,28 +1,91 @@
+import numpy as np
 import pytest
 
-from diffusion_for_multi_scale_molecular_dynamics.active_learning_loop.trainer.flare_hyperparameter_optimizer import \
-    FlareHyperparametersOptimizer
+from diffusion_for_multi_scale_molecular_dynamics.active_learning_loop.trainer.flare_hyperparameter_optimizer import (
+    FlareHyperparametersOptimizer, FlareOptimizerConfiguration,
+    HyperparameterTranslator)
 from tests.active_learning_loop.trainer.base_test_flare import BaseTestFlare
 
 
 class TestFlareHyperparameterOptimizer(BaseTestFlare):
 
+    @pytest.fixture(params=[True])
+    def optimize_sigma(self, request):
+        return request.param
+
+    @pytest.fixture(params=[True, False])
+    def optimize_sigma_e(self, request):
+        return request.param
+
+    @pytest.fixture(params=[True, False])
+    def optimize_sigma_f(self, request):
+        return request.param
+
+    @pytest.fixture(params=[True, False])
+    def optimize_sigma_s(self, request):
+        return request.param
+
+    @pytest.fixture()
+    def number_of_parameters_to_optimize(self, optimize_sigma, optimize_sigma_e, optimize_sigma_f, optimize_sigma_s):
+        return optimize_sigma + optimize_sigma_e + optimize_sigma_f + optimize_sigma_s
+
+    @pytest.fixture()
+    def starting_hyperparameters(self):
+        return np.random.rand(4)
+
+    @pytest.fixture()
+    def minimization_input(self, number_of_parameters_to_optimize):
+        return np.random.rand(number_of_parameters_to_optimize)
+
+    @pytest.fixture()
+    def expected_hyperparameters(self, starting_hyperparameters, minimization_input,
+                                 optimize_sigma, optimize_sigma_e, optimize_sigma_f, optimize_sigma_s):
+
+        new_parameters = 1.0 * starting_hyperparameters
+        position = 0
+        for idx, flag in enumerate([optimize_sigma, optimize_sigma_e, optimize_sigma_f, optimize_sigma_s]):
+            if flag:
+                new_parameters[idx] = minimization_input[position]
+                position += 1
+
+        return new_parameters
+
+    @pytest.fixture()
+    def translator(self, optimize_sigma, optimize_sigma_e, optimize_sigma_f, optimize_sigma_s):
+        return HyperparameterTranslator(optimize_sigma, optimize_sigma_e, optimize_sigma_f, optimize_sigma_s)
+
+    def test_generate_sgp_hyperparameters_from_minimization_inputs(self, translator, starting_hyperparameters,
+                                                                   minimization_input, expected_hyperparameters):
+        computed_hyperparameters = (
+            translator.generate_sgp_hyperparameters_from_minimization_inputs(starting_hyperparameters,
+                                                                             minimization_input))
+        np.testing.assert_allclose(expected_hyperparameters, computed_hyperparameters)
+
     @pytest.fixture(params=["BFGS", "L-BFGS-B", "nelder-mead"])
-    def method(self, request):
+    def optimization_method(self, request):
         return request.param
 
     @pytest.fixture
     def number_of_iterations(self):
         return 20
 
+    @pytest.fixture
+    def configuration(self, optimization_method, number_of_iterations,
+                      optimize_sigma, optimize_sigma_e, optimize_sigma_f, optimize_sigma_s):
+        return FlareOptimizerConfiguration(optimization_method=optimization_method,
+                                           max_optimization_iterations=number_of_iterations,
+                                           optimize_sigma=optimize_sigma,
+                                           optimize_sigma_e=optimize_sigma_e,
+                                           optimize_sigma_f=optimize_sigma_f,
+                                           optimize_sigma_s=optimize_sigma_s)
+
     @pytest.fixture()
-    def optimizer(self, method, number_of_iterations):
-        minimize_options = {"disp": False, "ftol": 1e-8, "gtol": 1e-8, "maxiter": number_of_iterations}
-        optimizer = FlareHyperparametersOptimizer(method, minimize_options)
-        return optimizer
+    def optimizer(self, configuration):
+        return FlareHyperparametersOptimizer(flare_optimizer_configuration=configuration)
 
     def test_smoke_flare_hyperparameter_optimizer(self, optimizer, flare_configuration,
-                                                  labelled_structure, active_environment_indices, number_of_iterations):
+                                                  number_of_parameters_to_optimize, labelled_structure,
+                                                  active_environment_indices, number_of_iterations):
 
         flare_trainer = self.instantiate_flare_trainer(flare_configuration,
                                                        labelled_structure,
