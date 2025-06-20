e1b7fd215671fe3163819a35401f00dae76db831
Bruno Rousseau <rousseau.bruno@gmail.com>
2025-06-20 12:44:44 -0400

    Fix excise and no op sample maker.

--- src/diffusion_for_multi_scale_molecular_dynamics/active_learning_loop/sample_maker/excise_and_noop_sample_maker.py
+++ src/diffusion_for_multi_scale_molecular_dynamics/active_learning_loop/sample_maker/excise_and_noop_sample_maker.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List
+from typing import Any, Dict, List, Tuple
 
 from diffusion_for_multi_scale_molecular_dynamics.active_learning_loop.sample_maker.base_sample_maker import (
     BaseExciseSampleMaker, BaseExciseSampleMakerArguments)
@@ -9,7 +9,6 @@ from diffusion_for_multi_scale_molecular_dynamics.namespace import AXL
 @dataclass(kw_only=True)
 class ExciseAndNoOpSampleMakerArguments(BaseExciseSampleMakerArguments):
     """Parameters for a trivial sample maker method."""
-
     algorithm: str = "excise_and_noop"
 
 
@@ -19,19 +18,28 @@ class ExciseAndNoOpSampleMaker(BaseExciseSampleMaker):
     def make_samples_from_constrained_substructure(
         self,
         substructure: AXL,
+        active_atom_index: int,
         num_samples: int = 1,
-    ) -> List[AXL]:
+    ) -> Tuple[List[AXL], List[int], List[Dict[str, Any]]]:
         """Create new samples using a constrained structure.
 
+        Since this is a "no op" implementation, the output will simply be the input.
+
         Args:
-            substructure: constrained atoms described as an AXL
+            substructure: constrained atoms described as an AXL.
+            active_atom_index: index of the "active atom" in the input substructure.
             num_samples: number of samples to make. Defaults to 1.
 
         Returns:
-            list of samples created. The length of the list should match num_samples.
-            list of additional information on samples created.
+            list_samples: the list of created samples. The length of the list should match num_samples.
+            list_active_atom_indices: for each created sample, the index of the "active atom", ie the
+                atom at the center of the excised region.
+            list_info: list of samples additional information.
         """
-        return [substructure] * num_samples, [{}] * num_samples
+        list_samples = num_samples * [substructure]
+        list_active_atom_indices = num_samples * [active_atom_index]
+        list_info = num_samples * [{}]
+        return list_samples, list_active_atom_indices, list_info
 
     def filter_made_samples(self, structures: List[AXL]) -> List[AXL]:
         """Return identical structures."""
