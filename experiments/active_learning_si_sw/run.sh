19605cab84edf2afeb375fa1d3cebfd04aa39bab
Bruno Rousseau <rousseau.bruno@gmail.com>
2025-06-20 19:04:28 -0400

    Update run script and config.

--- experiments/active_learning_si_sw/run.sh
+++ experiments/active_learning_si_sw/run.sh
@@ -14,18 +14,29 @@ LAMMPS_EXEC=${SRC_DIR}/lammps/build/lmp
 ARTN_PLUGIN=${SRC_DIR}/artn-plugin/build/libartn.dylib
 INITIAL_CHECKPOINT=./output/initial_trained_flare.json
 
-mkdir ./output/
 
+# NoOp
+CONFIG=noop_config.yaml 
+TOP_DIR=./noop/
+
+
+# Excise and NoOp
+#CONFIG=excise_and_noop_config.yaml 
+#TOP_DIR=./excise_and_noop/
+
+
+
+mkdir  $TOP_DIR
 # This creates the initial checkpoint
 python create_initial_checkpoint.py --path_to_lammps_executable $LAMMPS_EXEC \
                                     --path_to_initial_flare_checkpoint $INITIAL_CHECKPOINT
 
 for run in 1 2 3 4 5; do
-	OUTPUT_DIR=./output/run${run}
-	train_active_learning --config active_learning_config.yaml \
-			                  --path_to_reference_directory  ./reference \
-			                  --path_to_lammps_executable $LAMMPS_EXEC \
-			                  --path_to_artn_library_plugin $ARTN_PLUGIN \
-			                  --path_to_initial_flare_checkpoint $INITIAL_CHECKPOINT \
-			                  --output_directory $OUTPUT_DIR
+	OUTPUT_DIR=${TOP_DIR}/run${run}
+	train_active_learning --config $CONFIG \
+			      --path_to_reference_directory ./reference \
+			      --path_to_lammps_executable $LAMMPS_EXEC \
+			      --path_to_artn_library_plugin $ARTN_PLUGIN \
+			      --path_to_initial_flare_checkpoint $INITIAL_CHECKPOINT \
+			      --output_directory $OUTPUT_DIR
 done
