a618de995717f4df95537d918f3015c74f45b212
Bruno Rousseau <rousseau.bruno@gmail.com>
2025-06-20 13:01:04 -0400

    More fine grained factory control.

--- src/diffusion_for_multi_scale_molecular_dynamics/active_learning_loop/atom_selector/atom_selector_factory.py
+++ src/diffusion_for_multi_scale_molecular_dynamics/active_learning_loop/atom_selector/atom_selector_factory.py
@@ -1,7 +1,7 @@
 from typing import Any, AnyStr, Dict
 
-from diffusion_for_multi_scale_molecular_dynamics.active_learning_loop.atom_selector.base_atom_selector import \
-    BaseAtomSelector
+from diffusion_for_multi_scale_molecular_dynamics.active_learning_loop.atom_selector.base_atom_selector import (
+    BaseAtomSelector, BaseAtomSelectorParameters)
 from diffusion_for_multi_scale_molecular_dynamics.active_learning_loop.atom_selector.threshold_atom_selector import (
     ThresholdAtomSelector, ThresholdAtomSelectorParameters)
 from diffusion_for_multi_scale_molecular_dynamics.active_learning_loop.atom_selector.top_k_atom_selector import (
@@ -18,16 +18,17 @@ ATOM_SELECTOR_BY_NAME = dict(
 )
 
 
-def create_atom_selector(atom_selector_parameter_dictionary: Dict[AnyStr, Any]) -> BaseAtomSelector:
-    """Create atom selector.
+def create_atom_selector_parameters(atom_selector_parameter_dictionary: Dict[AnyStr, Any]) \
+        -> BaseAtomSelectorParameters:
+    """Create atom selector parameters.
 
-    This factor method is responsible for creating an atom selector based on configuration parameters.
+    This factory method is responsible for creating an atom selector configuration based on configuration parameters.
 
     Args:
         atom_selector_parameter_dictionary: a dictionary of relevant parameters to instantiate an atom selector.
 
     Returns:
-        atom_selector: a instance of an atom selector.
+        atom_selector_parameters: a configured atom selector parameter object.
     """
     assert "algorithm" in atom_selector_parameter_dictionary, "The algorithm is missing."
     algorithm = atom_selector_parameter_dictionary["algorithm"]
@@ -38,6 +39,19 @@ def create_atom_selector(atom_selector_parameter_dictionary: Dict[AnyStr, Any]) 
         f"Possible choices are {ATOM_SELECTOR_PARAMETERS_BY_NAME.keys()}")
 
     atom_selector_parameters = ATOM_SELECTOR_PARAMETERS_BY_NAME[algorithm](**atom_selector_parameter_dictionary)
-    atom_selector = ATOM_SELECTOR_BY_NAME[algorithm](atom_selector_parameters)
+    return atom_selector_parameters
 
+
+def create_atom_selector(atom_selector_parameters: BaseAtomSelectorParameters) -> BaseAtomSelector:
+    """Create atom selector.
+
+    This factor method is responsible for creating an atom selector based on configuration parameters.
+
+    Args:
+        atom_selector_parameters: a configured atom selector parameter object.
+
+    Returns:
+        atom_selector: an instance of an atom selector.
+    """
+    atom_selector = ATOM_SELECTOR_BY_NAME[atom_selector_parameters.algorithm](atom_selector_parameters)
     return atom_selector
