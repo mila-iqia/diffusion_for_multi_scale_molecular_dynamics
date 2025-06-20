2ffd72a9638f468256a721233d0754ef04dec7db
Bruno Rousseau <rousseau.bruno@gmail.com>
2025-06-20 12:43:19 -0400

    Fix no op sample maker.

--- src/diffusion_for_multi_scale_molecular_dynamics/active_learning_loop/sample_maker/no_op_sample_maker.py
+++ src/diffusion_for_multi_scale_molecular_dynamics/active_learning_loop/sample_maker/no_op_sample_maker.py
@@ -3,8 +3,8 @@ from typing import Any, Dict, List, Tuple
 
 import numpy as np
 
-from diffusion_for_multi_scale_molecular_dynamics.active_learning_loop.excisor.no_op_excisor import \
-    NoOpExcision
+from diffusion_for_multi_scale_molecular_dynamics.active_learning_loop.atom_selector.base_atom_selector import \
+    BaseAtomSelector
 from diffusion_for_multi_scale_molecular_dynamics.active_learning_loop.sample_maker.base_sample_maker import (
     BaseSampleMaker, BaseSampleMakerArguments)
 from diffusion_for_multi_scale_molecular_dynamics.namespace import AXL
@@ -25,10 +25,9 @@ class NoOpSampleMaker(BaseSampleMaker):
     This is a trivial sample maker that reproduces the input structures without modification.
     An Excisor is still needed in order to identify the active environments.
     """
-    def __init__(self, sample_maker_arguments: BaseSampleMakerArguments, no_op_excisor: NoOpExcision):
+    def __init__(self, sample_maker_arguments: BaseSampleMakerArguments, atom_selector: BaseAtomSelector):
         """Init method."""
-        super().__init__(sample_maker_arguments)
-        self.no_op_excisor = no_op_excisor
+        super().__init__(sample_maker_arguments, atom_selector)
 
     def make_samples(
         self,
@@ -36,8 +35,8 @@ class NoOpSampleMaker(BaseSampleMaker):
         uncertainty_per_atom: np.array,
     ) -> Tuple[List[AXL], List[np.array], List[Dict[str, Any]]]:
         """Noop make samples."""
-        active_environment_indices = self.no_op_excisor.select_central_atoms(uncertainty_per_atom)
-        return [structure], [active_environment_indices], [{}]
+        central_atom_indices = self.atom_selector.select_central_atoms(uncertainty_per_atom)
+        return [structure], [central_atom_indices], [{}]
 
     def filter_made_samples(self, structures: List[AXL]) -> List[AXL]:
         """Noop filter samples."""
