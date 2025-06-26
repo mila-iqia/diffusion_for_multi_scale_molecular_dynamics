8a8680b68421d90d05c9e766c46de2abdda41377
Bruno Rousseau <rousseau.bruno@gmail.com>
2025-06-26 11:58:27 -0400

    Cleaner run script.

--- experiments/active_learning_si_sw/run.sh
+++ experiments/active_learning_si_sw/run.sh
@@ -14,10 +14,11 @@ LAMMPS_EXEC=${SRC_DIR}/lammps/build/lmp
 ARTN_PLUGIN=${SRC_DIR}/artn-plugin/build/libartn.dylib
 INITIAL_CHECKPOINT=./output/initial_trained_flare.json
 
+list_runs="1 2 3 4 5"
 
 # NoOp
-#CONFIG=noop_config.yaml 
-#TOP_DIR=./noop/
+CONFIG=noop_config.yaml 
+TOP_DIR=./noop/
 
 
 # Excise and NoOp
@@ -25,15 +26,16 @@ INITIAL_CHECKPOINT=./output/initial_trained_flare.json
 #TOP_DIR=./excise_and_noop/
 
 # Excise and Random
-CONFIG=excise_and_random_config.yaml 
-TOP_DIR=./excise_and_random/
+#CONFIG=excise_and_random_config.yaml 
+#TOP_DIR=./excise_and_random/
 
 mkdir  $TOP_DIR
 # This creates the initial checkpoint
 python create_initial_checkpoint.py --path_to_lammps_executable $LAMMPS_EXEC \
                                     --path_to_initial_flare_checkpoint $INITIAL_CHECKPOINT
 
-for run in 1 2 3 4 5; do
+
+for run in $list_runs; do
 	OUTPUT_DIR=${TOP_DIR}/run${run}
 	train_active_learning --config $CONFIG \
 			      --path_to_reference_directory ./reference \
