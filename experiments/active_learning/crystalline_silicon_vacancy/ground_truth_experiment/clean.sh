2bf962023f524f83737e830e76a43d322d0c52e3
Bruno Rousseau <rousseau.bruno@gmail.com>
2025-07-11 09:44:56 -0400

    Clean up the ground truth experiment to use the reference!

--- experiments/active_learning/crystalline_silicon_vacancy/ground_truth_experiment/clean.sh
+++ experiments/active_learning/crystalline_silicon_vacancy/ground_truth_experiment/clean.sh
@@ -1,2 +1,2 @@
-rm artn.out dump.yaml initp.xyz log.lammps uncertain_dump.yaml latest_eigenvec.xyz min1.xyz  min2.xyz  mincounter  sad1.xyz  sadcounter
+rm -rf run*
 
--- experiments/active_learning/crystalline_silicon_vacancy/ground_truth_experiment/launch_many_runs.sh
+++ experiments/active_learning/crystalline_silicon_vacancy/ground_truth_experiment/launch_many_runs.sh
@@ -1,8 +1,17 @@
 export OMP_NUM_THREADS=4
+
+REFERENCE_DIR=../../reference/
+
+
 for run_id in {1..10}; do
     mkdir run"$run_id"
-    cp artn.in clean.sh conf.sw lammps.in Si.sw run"$run_id"/
+
+    cp lammps.in Si.sw run"$run_id"/
     cd  run"$run_id"
+
+    ln -s $REFERENCE_DIR/artn.in artn.in
+    ln -s $REFERENCE_DIR/initial_configuration.dat initial_configuration.dat
+
     mpirun -np 4 /Users/brunorousseau/sources/lammps/build/lmp < lammps.in
     cd ..
 done
