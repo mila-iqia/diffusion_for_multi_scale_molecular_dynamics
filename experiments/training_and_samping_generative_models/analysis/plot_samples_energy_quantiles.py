17fa66a1060fcb2222d1402ccc3014660f3a74a3
Bruno Rousseau <bruno.rousseau@mail.quebec>
2025-07-31 09:18:48 -0400

    Update plotting script.

--- experiments/training_and_samping_generative_models/analysis/plot_samples_energy_quantiles.py
+++ experiments/training_and_samping_generative_models/analysis/plot_samples_energy_quantiles.py
@@ -5,8 +5,11 @@ import numpy as np
 import torch
 from matplotlib import pyplot as plt
 
-from diffusion_for_multi_scale_molecular_dynamics.analysis import \
-    PLEASANT_FIG_SIZE
+from diffusion_for_multi_scale_molecular_dynamics.analysis import (
+    PLEASANT_FIG_SIZE, PLOT_STYLE_PATH)
+
+plt.style.use(PLOT_STYLE_PATH)
+plt.rc('font', size=16)
 
 artifact_directory = Path(__file__).parent / "data_artifacts"
 image_directory = Path(__file__).parent / "images"
@@ -27,13 +30,13 @@ match dataset_name:
     case "SiGe_diffusion_1x1x1":
         sample_top_dir = base_experiment_dir / "july26_sige_egnn_1x1x1/samples_from_run1/"
     case "Si_diffusion_2x2x2":
-        experiment_dir = base_experiment_dir / "july26_si_egnn_2x2x2/samples_from_run1/"
+        sample_top_dir = base_experiment_dir / "july26_si_egnn_2x2x2/samples_from_run1/"
     case "SiGe_diffusion_2x2x2":
-        experiment_dir = base_experiment_dir / "july26_sige_egnn_2x2x2/samples_from_run1/"
+        sample_top_dir = base_experiment_dir / "july26_sige_egnn_2x2x2/samples_from_run1/"
     case "Si_diffusion_3x3x3":
-        experiment_dir = base_experiment_dir / "july26_si_egnn_3x3x3/samples_from_run1/"
+        sample_top_dir = base_experiment_dir / "july26_si_egnn_3x3x3/samples_from_run1/"
     case "SiGe_diffusion_3x3x3":
-        experiment_dir = base_experiment_dir / "july26_sige_egnn_3x3x3/samples_from_run1/"
+        sample_top_dir = base_experiment_dir / "july26_sige_egnn_3x3x3/samples_from_run1/"
 
 
 list_T = [1000, 10000]
@@ -48,11 +51,16 @@ if __name__ == "__main__":
     dataset_quantiles = np.quantile(reference_energies, list_q)
 
     fig = plt.figure(figsize=PLEASANT_FIG_SIZE)
-    fig.suptitle(f"Sampling Energy Quantiles\n{dataset_name}")
+    dataset_name_for_title = dataset_name.replace("_diffusion_", " ")
+    fig.suptitle(f"Sampling Energy Quantiles\n{dataset_name_for_title}")
     ax = fig.add_subplot(111)
 
     label = "Validation Dataset"
     ax.plot(100 * list_q, dataset_quantiles, "--", lw=5, color="green", label=label)
+
+    yrange = dataset_quantiles.max() - dataset_quantiles.min()
+    ymax = dataset_quantiles.max() + 0.25 * yrange
+    ymin = dataset_quantiles.min() - 0.25 * yrange
 
     for T in list_T:
         sample_energies = torch.load(sample_top_dir / f'sample_output_T={T}' / "energies.pt")
@@ -65,8 +73,8 @@ if __name__ == "__main__":
     ax.set_xlabel("Quantile (%)")
     ax.set_ylabel("Energy (eV)")
     ax.set_xlim(-0.1, 100.1)
+    ax.set_ylim(ymin=ymin, ymax=ymax)
     ax.legend(loc="upper left", fancybox=True, shadow=True, ncol=1, fontsize=12)
     fig.tight_layout()
-
     fig.savefig(image_directory / f"sample_energy_quantiles_{dataset_name}.png")
     plt.close(fig)
