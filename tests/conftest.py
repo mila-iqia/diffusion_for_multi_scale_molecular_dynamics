38769211121a5f2745add94bc89d1934aedff50b
Olivier7017 <olivier.nadeau2@uqtr.ca>
2026-05-11 17:19:50 -0400

    isort

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 from functools import lru_cache
 
 import numpy as np
@@ -9,7 +10,6 @@ from tests.fake_data_utils import (create_dump_yaml_documents,
                                    generate_random_string,
                                    get_configuration_runs, write_to_yaml)
 
-import os
 os.environ.setdefault("TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD", "true")
 
 
