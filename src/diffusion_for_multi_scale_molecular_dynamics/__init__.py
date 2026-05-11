864b1dc83b522a0d00c0cc96820708b9e8c295d7
Olivier7017 <olivier.nadeau2@uqtr.ca>
2026-05-11 15:37:22 -0400

    Fixing mace-torch e3nn deps

--- src/diffusion_for_multi_scale_molecular_dynamics/__init__.py
+++ src/diffusion_for_multi_scale_molecular_dynamics/__init__.py
@@ -1,4 +1,10 @@
+import os
 from pathlib import Path
+
+# mace-torch pins e3nn==0.4.4 which is too old to use weights_only=False.
+# This is necessary in torch.load() for torch>=2.6.0
+# We workaround using this environment variable.
+os.environ.setdefault("TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD", "true")
 
 ROOT_DIR = Path(__file__).parent.parent
 TOP_DIR = ROOT_DIR.parent
